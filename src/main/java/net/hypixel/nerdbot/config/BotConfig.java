60f92718f9999c793fbd126ebd5adc2b6b59432f
Aerh <22944369+Aerhhh@users.noreply.github.com>
2022-04-17 20:01:26 +0100

    Add more fields to the config

--- src/main/java/net/hypixel/nerdbot/config/BotConfig.java
+++ src/main/java/net/hypixel/nerdbot/config/BotConfig.java
@@ -4,11 +4,40 @@ public class BotConfig {
 
     private String prefix;
 
+    private int minimumThreshold;
+
+    private double percentage;
+
     public String getPrefix() {
         return prefix;
     }
 
     public void setPrefix(String prefix) {
         this.prefix = prefix;
+    }
+
+    public int getMinimumThreshold() {
+        return minimumThreshold;
+    }
+
+    public void setMinimumThreshold(int minimumThreshold) {
+        this.minimumThreshold = minimumThreshold;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public String toString() {
+        return "BotConfig{" +
+                "prefix='" + prefix + '\'' +
+                ", minimumThreshold=" + minimumThreshold +
+                ", percentage=" + percentage +
+                '}';
     }
 }
