a6a539acd0292f5683f8a6dd66a67a3d609e236c
Callum <22944369+callumjmu@users.noreply.github.com>
2022-06-07 01:06:27 +0100

    Remove greenlit command, think of more public commands later

--- src/main/java/net/hypixel/nerdbot/command/PublicCommands.java
+++ src/main/java/net/hypixel/nerdbot/command/PublicCommands.java
@@ -8,15 +8,4 @@ import net.hypixel.nerdbot.database.Database;
 @AutoMapping
 public class PublicCommands {
 
-    @Command(name = "greenlit")
-    public void greenlit(CommandContext context) {
-        if (!Database.getInstance().isConnected()) {
-            context.getMessage().reply("An error occurred!").queue();
-            return;
-        }
-
-        int total = Database.getInstance().getGreenlitCollection().size();
-        context.getMessage().reply("There are currently **" + total + "** greenlit suggestions.").queue();
-    }
-
 }
