1e40e3baa45878e17ed54b3bd29b0c5ede0fb45c
Aerh <22944369+Aerhhh@users.noreply.github.com>
2022-08-21 13:56:25 +0100

    Lombok (#11)
    * Add Lombok 1.18.24
    
    * Convert a bunch of stuff to Lombok
    
    * Convert a bunch of stuff to Lombok

--- src/main/java/net/hypixel/nerdbot/api/database/DiscordUser.java
+++ src/main/java/net/hypixel/nerdbot/api/database/DiscordUser.java
@@ -1,10 +1,15 @@
 package net.hypixel.nerdbot.api.database;
 
-import org.jetbrains.annotations.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class DiscordUser {
 
     private String discordId;
@@ -12,45 +17,5 @@ public class DiscordUser {
     private List<String> agrees, disagrees;
 
     public DiscordUser() {
-    }
-
-    public DiscordUser(String discordId, Date lastKnownActivityDate, List<String> agrees, List<String> disagrees) {
-        this.discordId = discordId;
-        this.lastKnownActivityDate = lastKnownActivityDate;
-        this.agrees = agrees;
-        this.disagrees = disagrees;
-    }
-
-    public String getDiscordId() {
-        return discordId;
-    }
-
-    public void setDiscordId(String discordId) {
-        this.discordId = discordId;
-    }
-
-    @Nullable
-    public Date getLastKnownActivityDate() {
-        return lastKnownActivityDate;
-    }
-
-    public void setLastKnownActivityDate(@Nullable Date lastKnownActivityDate) {
-        this.lastKnownActivityDate = lastKnownActivityDate;
-    }
-
-    public List<String> getAgrees() {
-        return agrees;
-    }
-
-    public void setAgrees(List<String> agrees) {
-        this.agrees = agrees;
-    }
-
-    public List<String> getDisagrees() {
-        return disagrees;
-    }
-
-    public void setDisagrees(List<String> disagrees) {
-        this.disagrees = disagrees;
     }
 }
