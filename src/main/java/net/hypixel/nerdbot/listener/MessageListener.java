69f01bc492b0028427c956b1e3354c70bc90081f
callumjmu <aerh.yt@gmail.com>
2022-09-04 19:34:41 +0100

    Not really read only but also yes

--- src/main/java/net/hypixel/nerdbot/listener/MessageListener.java
+++ src/main/java/net/hypixel/nerdbot/listener/MessageListener.java
@@ -24,11 +24,6 @@ public class MessageListener implements EventListener {
             if (messageEvent.getAuthor().isBot() && !messageEvent.getAuthor().getId().equals(NerdBotApp.getBot().getJDA().getSelfUser().getId()))
                 return;
 
-            if (System.getProperty("bot.readOnly") != null && Boolean.parseBoolean(System.getProperty("bot.readOnly"))) {
-                Logger.info("Read only mode is enabled, ignoring new message");
-                return;
-            }
-
             Guild guild = messageEvent.getGuild();
             List<ChannelGroup> groups = Database.getInstance().getChannelGroups();
             if (groups == null || groups.isEmpty()) return;
@@ -46,6 +41,12 @@ public class MessageListener implements EventListener {
                 if (!group.getFrom().equals(channel.getId())) continue;
 
                 message.createThreadChannel("[Discussion] " + Util.getFirstLine(message)).queue(threadChannel -> threadChannel.addThreadMember(message.getAuthor()).queue());
+
+                if (System.getProperty("bot.readOnly") != null && Boolean.parseBoolean(System.getProperty("bot.readOnly"))) {
+                    Logger.info("Read-only mode is enabled, not adding reactions to new message " + message.getId());
+                    return;
+                }
+
                 message.addReaction(yes).queue();
                 message.addReaction(no).queue();
             }
