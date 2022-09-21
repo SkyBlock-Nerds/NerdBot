b6af76a75d50d4be0cd36bb3340b4abcad241308
callumjmu <aerh.yt@gmail.com>
2022-09-21 01:23:04 +0100

    Add a readOnly option for the curator

--- src/main/java/net/hypixel/nerdbot/curator/Curator.java
+++ src/main/java/net/hypixel/nerdbot/curator/Curator.java
@@ -10,11 +10,20 @@ import java.util.List;
 public abstract class Curator<T> {
 
     private long startTime, endTime;
+    private final boolean readOnly;
+
+    protected Curator(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
 
     public abstract List<GreenlitMessage> curate(List<T> list);
 
     public void log(String message) {
         Logger.info("[" + super.getClass().getSimpleName() + "] " + message);
+    }
+
+    public void error(String message) {
+        Logger.error("[" + super.getClass().getSimpleName() + "] " + message);
     }
 
     public double getRatio(int positiveReactions, int negativeReactions) {
@@ -38,6 +47,10 @@ public abstract class Curator<T> {
 
     public void setEndTime(long endTime) {
         this.endTime = endTime;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
     }
 
     public Bot getBot() {
