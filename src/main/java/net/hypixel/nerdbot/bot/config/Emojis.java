73092e08917c7118e507eaa3098130009f024d73
Aerh <22944369+Aerhhh@users.noreply.github.com>
2022-10-15 01:24:33 +0100

    Add a couple commands for config stuff (#17)
    Config commands (#17)

--- src/main/java/net/hypixel/nerdbot/bot/config/Emojis.java
+++ src/main/java/net/hypixel/nerdbot/bot/config/Emojis.java
@@ -3,10 +3,12 @@ package net.hypixel.nerdbot.bot.config;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Emojis {
 
     public static final Emojis DEFAULT = new Emojis("\uD83D\uDC4D", "\uD83D\uDC4E", "\u2705");
