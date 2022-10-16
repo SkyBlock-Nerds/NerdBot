3e1dfea8f7fabae51f49075c2f442e6f15db9512
Aerh <aerh.yt@gmaill.com>
2022-10-16 18:50:07 +0100

    Migrate /curate command

--- src/main/java/net/hypixel/nerdbot/command/CurateSlashCommand.java
+++ src/main/java/net/hypixel/nerdbot/command/CurateSlashCommand.java
@@ -1,14 +1,10 @@
 package net.hypixel.nerdbot.command;
 
-import net.aerh.jdacommands.command.CommandArgument;
-import net.aerh.jdacommands.command.HasArguments;
-import net.aerh.jdacommands.command.RequiresPermission;
-import net.aerh.jdacommands.command.SlashCommand;
-import net.dv8tion.jda.api.Permission;
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.annotations.AppOption;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.hypixel.nerdbot.NerdBotApp;
 import net.hypixel.nerdbot.api.curator.Curator;
 import net.hypixel.nerdbot.api.database.Database;
@@ -16,49 +12,18 @@ import net.hypixel.nerdbot.api.database.GreenlitMessage;
 import net.hypixel.nerdbot.curator.ForumChannelCurator;
 
 import java.util.List;
-import java.util.Objects;
 
-public class CurateSlashCommand implements SlashCommand, RequiresPermission, HasArguments {
+public class CurateSlashCommand extends ApplicationCommand {
 
-    @Override
-    public String getCommandName() {
-        return "curate";
-    }
-
-    @Override
-    public String getDescription() {
-        return "Manually start the suggestion curation process";
-    }
-
-    @Override
-    public List<CommandArgument> getArguments() {
-        return List.of(
-                CommandArgument.of(OptionType.CHANNEL, "forum", "The forum channel to curate", true),
-                CommandArgument.of(OptionType.BOOLEAN, "readonly", "Whether this run should be done in read-only mode", false)
-        );
-    }
-
-    @Override
-    public DefaultMemberPermissions permissionRequired() {
-        return DefaultMemberPermissions.enabledFor(Permission.BAN_MEMBERS);
-    }
-
-    @Override
-    public void execute(SlashCommandInteractionEvent event) {
+    @JDASlashCommand(name = "curate", description = "Manually run the curation process", defaultLocked = true)
+    public void curate(GuildSlashEvent event, ForumChannel forumChannel, @AppOption(description = "Run the curator without greenlighting suggestions") boolean readOnly) {
         if (!Database.getInstance().isConnected()) {
             event.reply("Couldn't connect to the database!").setEphemeral(true).queue();
             NerdBotApp.LOGGER.error("Couldn't connect to the database!");
             return;
         }
 
-        boolean readOnly = false;
-        if (event.getOption("readonly") != null) {
-            readOnly = Objects.requireNonNull(event.getOption("readonly")).getAsBoolean();
-        }
-
         Curator<ForumChannel> forumChannelCurator = new ForumChannelCurator(readOnly);
-        ForumChannel forumChannel = event.getOption("forum").getAsChannel().asForumChannel();
-
         NerdBotApp.EXECUTOR_SERVICE.submit(() -> {
             event.deferReply(true).queue();
 
