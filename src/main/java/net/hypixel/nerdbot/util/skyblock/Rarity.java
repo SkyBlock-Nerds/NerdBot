b3b36f87e4137211b0b546dc6210d3b54353407f
IndigoPolecat <115671621+IndigoPolecat@users.noreply.github.com>
2023-01-08 01:21:12 -0700

    SUPREMEEEEEEEEEEEEEEEEEE

--- src/main/java/net/hypixel/nerdbot/util/skyblock/Rarity.java
+++ src/main/java/net/hypixel/nerdbot/util/skyblock/Rarity.java
@@ -10,6 +10,7 @@ public enum Rarity {
     DIVINE("DIVINE", MCColor.AQUA),
     SPECIAL("SPECIAL", MCColor.RED),
     VERY_SPECIAL("VERY SPECIAL", MCColor.RED),
+    SUPREME("SUPREME", MCColor.DARK_RED),
     NONE("", MCColor.GRAY);
 
     public static final Rarity[] VALUES = values();
