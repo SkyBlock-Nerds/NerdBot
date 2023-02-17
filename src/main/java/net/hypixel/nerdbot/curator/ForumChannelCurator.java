2534c2610c1a740e55adb419321142db816fcf8f
Aerh <aerh.yt@gmail.com>
2023-02-17 04:00:46 +0000

    start debugging greenlit stuff

--- src/main/java/net/hypixel/nerdbot/curator/ForumChannelCurator.java
+++ src/main/java/net/hypixel/nerdbot/curator/ForumChannelCurator.java
@@ -2,25 +2,16 @@ package net.hypixel.nerdbot.curator;
 
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
-import net.dv8tion.jda.api.entities.channel.forums.BaseForumTag;
-import net.dv8tion.jda.api.entities.channel.forums.ForumTag;
-import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.exceptions.RateLimitedException;
-import net.dv8tion.jda.internal.entities.ForumTagImpl;
 import net.hypixel.nerdbot.NerdBotApp;
-import net.hypixel.nerdbot.channel.ChannelManager;
 import net.hypixel.nerdbot.api.curator.Curator;
 import net.hypixel.nerdbot.api.database.Database;
 import net.hypixel.nerdbot.api.database.greenlit.GreenlitMessage;
-import net.hypixel.nerdbot.api.database.user.DiscordUser;
-import net.hypixel.nerdbot.util.discord.Users;
-import net.hypixel.nerdbot.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Log4j2
 public class ForumChannelCurator extends Curator<ForumChannel> {
@@ -53,70 +44,12 @@ public class ForumChannelCurator extends Curator<ForumChannel> {
         log.info("Found " + threads.size() + " non-greenlit forum post(s)!");
 
         for (ThreadChannel thread : threads) {
-            log.info("[" + (index++) + "/" + threads.size() + "] Curating thread: " + thread.getName() + " (Thread ID: " + thread.getId() + ")");
+            log.info("Curating thread '" + thread.getName() + "' (ID: " + thread.getId() + ") by " + thread.getOwner().getEffectiveName());
 
-            try {
-                // This is a stupid way to do it but it's the only way that works right now
-                List<Message> allMessages = thread.getIterableHistory().complete(true);
-                Message firstPost = allMessages.get(allMessages.size() - 1);
-                Emoji agreeEmoji = getJDA().getEmojiById(NerdBotApp.getBot().getConfig().getEmojiConfig().getAgreeEmojiId());
-                Emoji disagreeEmoji = getJDA().getEmojiById(NerdBotApp.getBot().getConfig().getEmojiConfig().getDisagreeEmojiId());
-                Emoji neutralEmoji = getJDA().getEmojiById(NerdBotApp.getBot().getConfig().getEmojiConfig().getNeutralEmojiId());
-                DiscordUser discordUser = Util.getOrAddUserToCache(database, firstPost.getAuthor().getId());
-                if (discordUser == null) {
-                    continue;
-                }
+            MessageHistory history = thread.getHistoryFromBeginning(1).complete();
+            Message message = history.getRetrievedHistory().get(0);
 
-                if (discordUser.getLastActivity().getLastSuggestionDate() < firstPost.getTimeCreated().toInstant().toEpochMilli()) {
-                    discordUser.getLastActivity().setLastSuggestionDate(firstPost.getTimeCreated().toInstant().toEpochMilli());
-                    log.info("Updating last suggestion time for " + firstPost.getAuthor().getName() + " to " + firstPost.getTimeCreated().toEpochSecond());
-                }
-
-                if (agreeEmoji == null || disagreeEmoji == null || neutralEmoji == null) {
-                    log.error("Couldn't find an emoji, time to yell!");
-                    if (ChannelManager.getLogChannel() != null) {
-                        ChannelManager.getLogChannel().sendMessage(Users.getUser(Users.AERH).getAsMention() + " I couldn't find an emoji for some reason, check logs!").queue();
-                    }
-                    break;
-                }
-
-                // Add the reactions if they're missing
-                // Theoretically the agree reaction should be there by default but lets check anyway just in case
-                int agreeReactions = firstPost.getReaction(agreeEmoji) == null ? 0 : Objects.requireNonNull(firstPost.getReaction(agreeEmoji)).getCount();
-                int disagreeReactions = firstPost.getReaction(disagreeEmoji) == null ? 0 : Objects.requireNonNull(firstPost.getReaction(disagreeEmoji)).getCount();
-                int neutralReactions = firstPost.getReaction(neutralEmoji) == null ? 0 : Objects.requireNonNull(firstPost.getReaction(neutralEmoji)).getCount();
-
-                if (agreeReactions < NerdBotApp.getBot().getConfig().getMinimumThreshold()) {
-                    log.info("Post " + firstPost.getId() + " doesn't have enough agree reactions, skipping...");
-                    continue;
-                }
-
-                // Get the ratio of reactions and greenlight it if it's over the threshold
-                double ratio = getRatio(agreeReactions, disagreeReactions, neutralReactions);
-                if (ratio >= NerdBotApp.getBot().getConfig().getPercentage()) {
-                    GreenlitMessage greenlitMessage = GreenlitMessage.builder()
-                            .agrees(agreeReactions)
-                            .disagrees(disagreeReactions)
-                            .messageId(firstPost.getId())
-                            .userId(firstPost.getAuthor().getId())
-                            .suggestionUrl(firstPost.getJumpUrl())
-                            .suggestionTitle(thread.getName())
-                            .suggestionTimestamp(thread.getTimeCreated().toInstant().toEpochMilli())
-                            .suggestionContent(firstPost.getContentRaw())
-                            .tags(thread.getAppliedTags().stream().map(BaseForumTag::getName).toList())
-                            .build();
-
-                    log.info("Greenlighting thread '" + thread.getName() + "' (Thread ID: " + thread.getId() + ") with a ratio of " + ratio + "%");
-                    List<ForumTag> tags = new ArrayList<>(thread.getAppliedTags());
-                    tags.add(new ForumTagImpl(Long.parseLong(NerdBotApp.getBot().getConfig().getTagConfig().getGreenlit())));
-                    thread.getManager().setAppliedTags(tags).queue();
-                    output.add(greenlitMessage);
-                    log.debug("Added " + greenlitMessage + " to curator output");
-                }
-            } catch (RateLimitedException exception) {
-                log.info("Currently being rate limited so the curation process has to stop!");
-                break;
-            }
+            log.info("Message: " + message.getContentDisplay());
         }
 
         setEndTime(System.currentTimeMillis());
