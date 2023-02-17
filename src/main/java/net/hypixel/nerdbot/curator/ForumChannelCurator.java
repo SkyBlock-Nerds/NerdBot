fdbde9e08d0dce91081864f35055e3e23bdc62aa
Aerh <aerh.yt@gmail.com>
2023-02-17 04:52:02 +0000

    I guess we'll try this

--- src/main/java/net/hypixel/nerdbot/curator/ForumChannelCurator.java
+++ src/main/java/net/hypixel/nerdbot/curator/ForumChannelCurator.java
@@ -1,11 +1,13 @@
 package net.hypixel.nerdbot.curator;
 
 import lombok.extern.log4j.Log4j2;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.hypixel.nerdbot.NerdBotApp;
 import net.hypixel.nerdbot.api.curator.Curator;
 import net.hypixel.nerdbot.api.database.Database;
@@ -18,16 +20,17 @@ import java.util.List;
 @Log4j2
 public class ForumChannelCurator extends Curator<ForumChannel> {
 
-    private final Database database = NerdBotApp.getBot().getDatabase();
-    private final EmojiConfig emojiConfig = NerdBotApp.getBot().getConfig().getEmojiConfig();
-
     public ForumChannelCurator(boolean readOnly) {
         super(readOnly);
     }
 
     @Override
     public List<GreenlitMessage> curate(ForumChannel forumChannel) {
+        Database database = NerdBotApp.getBot().getDatabase();
+        EmojiConfig emojiConfig = NerdBotApp.getBot().getConfig().getEmojiConfig();
+        Guild guild = NerdBotApp.getBot().getJDA().getGuildById(NerdBotApp.getBot().getConfig().getGuildId());
         List<GreenlitMessage> output = new ArrayList<>();
+
         setStartTime(System.currentTimeMillis());
 
         if (!database.isConnected()) {
@@ -47,33 +50,39 @@ public class ForumChannelCurator extends Curator<ForumChannel> {
         log.info("Found " + threads.size() + " non-greenlit forum post(s)!");
 
         for (ThreadChannel thread : threads) {
+            if (thread.getOwner() == null) {
+                guild.retrieveMemberById(thread.getOwnerId()).complete();
+            }
+
             log.info("Curating thread '" + thread.getName() + "' (ID: " + thread.getId() + ") by " + thread.getOwner().getEffectiveName());
 
             MessageHistory history = thread.getHistoryFromBeginning(1).complete();
             Message message = history.getRetrievedHistory().get(0);
-
             if (message == null) {
                 log.error("Message for thread '" + thread.getName() + "' (ID: " + thread.getId() + ") is null!");
                 return output;
             }
 
-            log.info("Found message: " + message.getContentRaw());
-
             try {
                 log.info("Checking reaction counts for message ID: " + message.getId());
 
+                List<MessageReaction> reactions = message.getReactions()
+                        .stream()
+                        .filter(reaction -> reaction.getEmoji().getType() == Emoji.Type.CUSTOM)
+                        .toList();
+
                 // Get agree reactions or default to 0
-                int agree = message.getReactions().stream()
+                int agree = reactions.stream()
                         .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getAgreeEmojiId()))
                         .mapToInt(MessageReaction::getCount)
                         .findFirst()
                         .orElse(0);
-                int disagree = message.getReactions().stream()
+                int disagree = reactions.stream()
                         .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getDisagreeEmojiId()))
                         .mapToInt(MessageReaction::getCount)
                         .findFirst()
                         .orElse(0);
-                int neutral = message.getReactions().stream()
+                int neutral = reactions.stream()
                         .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getNeutralEmojiId()))
                         .mapToInt(MessageReaction::getCount)
                         .findFirst()
