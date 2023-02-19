91d79a53fa1b564b99b564b6ed84cb825b6ca06a
Aerh <22944369+Aerhhh@users.noreply.github.com>
2023-02-19 02:49:52 +0000

    Condense commands into a few classes

---
+++ src/main/java/net/hypixel/nerdbot/command/InfoCommands.java
@@ -0,0 +1,203 @@
+package net.hypixel.nerdbot.command;
+
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.annotations.AppOption;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import lombok.extern.log4j.Log4j2;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.SelfUser;
+import net.hypixel.nerdbot.NerdBotApp;
+import net.hypixel.nerdbot.api.database.Database;
+import net.hypixel.nerdbot.api.database.greenlit.GreenlitMessage;
+import net.hypixel.nerdbot.api.database.user.DiscordUser;
+import net.hypixel.nerdbot.api.database.user.LastActivity;
+import net.hypixel.nerdbot.util.Environment;
+import net.hypixel.nerdbot.util.Time;
+import net.hypixel.nerdbot.util.Util;
+import net.hypixel.nerdbot.util.discord.DiscordTimestamp;
+
+import java.awt.*;
+import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Log4j2
+public class InfoCommands extends ApplicationCommand {
+
+    private final Database database = NerdBotApp.getBot().getDatabase();
+
+    @JDASlashCommand(name = "info", subcommand = "bot", description = "View information about the bot", defaultLocked = true)
+    public void botInfo(GuildSlashEvent event) {
+        StringBuilder builder = new StringBuilder();
+        SelfUser bot = NerdBotApp.getBot().getJDA().getSelfUser();
+        long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+
+        builder.append(" • Bot name: ").append(bot.getName()).append(" (ID: ").append(bot.getId()).append(")").append("\n")
+                .append("• Environment: ").append(Environment.getEnvironment()).append("\n")
+                .append("• Uptime: ").append(Time.formatMs(NerdBotApp.getBot().getUptime())).append("\n")
+                .append("• Memory: ").append(Util.formatSize(usedMemory)).append(" / ").append(Util.formatSize(totalMemory)).append("\n");
+
+        event.reply(builder.toString()).setEphemeral(true).queue();
+    }
+
+    @JDASlashCommand(name = "info", subcommand = "greenlit", description = "Get a list of all non-docced greenlit messages. May not be 100% accurate!", defaultLocked = true)
+    public void greenlitInfo(GuildSlashEvent event, @AppOption int page) {
+        List<GreenlitMessage> greenlit = database.getCollection("greenlit_messages", GreenlitMessage.class)
+                .find()
+                .into(new ArrayList<>())
+                .stream()
+                .filter(greenlitMessage -> greenlitMessage.getTags() != null && !greenlitMessage.getTags().contains("Docced"))
+                .collect(Collectors.toList());
+        List<GreenlitMessage> pages = getPage(greenlit, page, 10);
+        pages.sort(Comparator.comparingLong(GreenlitMessage::getSuggestionTimestamp));
+
+        StringBuilder stringBuilder = new StringBuilder("**Page " + page + "**\n");
+        if (pages.isEmpty()) {
+            stringBuilder.append("No results found");
+        } else {
+            for (GreenlitMessage greenlitMessage : pages) {
+                stringBuilder.append(" • [").append(greenlitMessage.getSuggestionTitle()).append("](").append(greenlitMessage.getSuggestionUrl()).append(")\n");
+            }
+        }
+
+        event.reply(stringBuilder.toString()).setEphemeral(true).queue();
+    }
+
+    @JDASlashCommand(name = "info", subcommand = "server", description = "View some information about the server", defaultLocked = true)
+    public void serverInfo(GuildSlashEvent event) {
+        Guild guild = event.getGuild();
+        StringBuilder builder = new StringBuilder();
+        int staff = guild.getMembersWithRoles(Util.getRole("Ultimate Nerd")).size()
+                + guild.getMembersWithRoles(Util.getRole("Ultimate Nerd But Red")).size()
+                + guild.getMembersWithRoles(Util.getRole("Game Master")).size();
+
+        builder.append("Server name: ").append(guild.getName()).append(" (Server ID: ").append(guild.getId()).append(")\n")
+                .append("Created at: ").append(new DiscordTimestamp(guild.getTimeCreated().toInstant().toEpochMilli()).toRelativeTimestamp()).append("\n")
+                .append("Boosters: ").append(guild.getBoostCount()).append(" (").append(guild.getBoostTier().name()).append(")\n")
+                .append("Channels: ").append(guild.getChannels().size()).append("\n")
+                .append("Members: ").append(guild.getMembers().size()).append("/").append(guild.getMaxMembers()).append("\n")
+                .append("  • Staff: ").append(staff).append("\n")
+                .append("  • HPC: ").append(guild.getMembersWithRoles(Util.getRole("HPC")).size()).append("\n")
+                .append("  • Grapes: ").append(guild.getMembersWithRoles(Util.getRole("Grape")).size()).append("\n")
+                .append("  • Nerds: ").append(guild.getMembersWithRoles(Util.getRole("Nerd")).size());
+
+        event.reply(builder.toString()).setEphemeral(true).queue();
+    }
+
+    @JDASlashCommand(name = "info", subcommand = "user", description = "View some information about a user", defaultLocked = true)
+    public void userInfo(GuildSlashEvent event, @AppOption(description = "The user to search") Member member) {
+        if (!database.isConnected()) {
+            event.reply("Couldn't connect to the database!").setEphemeral(true).queue();
+            return;
+        }
+
+        DiscordUser discordUser;
+        if (NerdBotApp.USER_CACHE.getIfPresent(member.getId()) != null) {
+            discordUser = NerdBotApp.USER_CACHE.getIfPresent(member.getId());
+        } else {
+            discordUser = database.findDocument(database.getCollection("users", DiscordUser.class), "discordId", member.getId()).first();
+        }
+
+        if (discordUser == null) {
+            event.reply("Couldn't find that user in the database!").setEphemeral(true).queue();
+            return;
+        }
+
+        LastActivity lastActivity = discordUser.getLastActivity();
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+
+        embedBuilder.setColor(Color.GREEN)
+                .setThumbnail(member.getEffectiveAvatarUrl())
+                .setTitle(member.getEffectiveName() + " (" + member.getId() + ")")
+                .setDescription("**Last Known Activity**")
+                .addField("Global", new DiscordTimestamp(lastActivity.getLastGlobalActivity()).toRelativeTimestamp(), true)
+                .addField("Alpha", new DiscordTimestamp(lastActivity.getLastAlphaActivity()).toRelativeTimestamp(), true)
+                .addField("Voice Chat", new DiscordTimestamp(lastActivity.getLastVoiceChannelJoinDate()).toRelativeTimestamp(), true)
+                .addField("Suggestions", new DiscordTimestamp(lastActivity.getLastSuggestionDate()).toRelativeTimestamp(), true)
+                .addField("Item Generator", new DiscordTimestamp(lastActivity.getLastItemGenUsage()).toRelativeTimestamp(), true)
+                .addBlankField(true);
+
+        event.replyEmbeds(embedBuilder.build()).setEphemeral(true).queue();
+    }
+
+    @JDASlashCommand(name = "info", subcommand = "activity", description = "View information regarding user activity", defaultLocked = true)
+    public void userActivityInfo(GuildSlashEvent event, @AppOption int page) {
+        if (!database.isConnected()) {
+            event.reply("Couldn't connect to the database!").setEphemeral(true).queue();
+            return;
+        }
+
+        List<DiscordUser> allUsers = database.getCollection("users", DiscordUser.class).find().into(new ArrayList<>());
+        List<DiscordUser> inactiveUsers = allUsers.stream().filter(discordUser -> discordUser.getLastActivity().getLastGlobalActivity() == -1L).toList();
+        List<DiscordUser> users = getPage(inactiveUsers, page, 25);
+        Map<String, Integer> roles = new HashMap<>();
+        StringBuilder stringBuilder = new StringBuilder("**Page " + page + "**\n");
+
+        System.out.println("Found " + inactiveUsers.size() + " inactive user" + (inactiveUsers.size() == 1 ? "" : "s") + "!");
+        // Do some logging stuff
+        for (DiscordUser all : inactiveUsers) {
+            Member member = NerdBotApp.getBot().getJDA().getGuildById(NerdBotApp.getBot().getConfig().getGuildId()).getMemberById(all.getDiscordId());
+            if (member == null) {
+                log.error("Couldn't find member " + all.getDiscordId());
+                continue;
+            }
+
+            if (member.getRoles().isEmpty()) {
+                log.info(member.getEffectiveName() + " doesn't have any roles!");
+                continue;
+            }
+
+            if (all.getLastActivity().getLastGlobalActivity() == -1) {
+                roles.put(member.getRoles().get(0).getName(), roles.getOrDefault(member.getRoles().get(0).getName(), 0) + 1);
+            }
+        }
+
+        // Create the page
+        for (DiscordUser user : users) {
+            Member member = NerdBotApp.getBot().getJDA().getGuildById(NerdBotApp.getBot().getConfig().getGuildId()).getMemberById(user.getDiscordId());
+            if (member == null) {
+                log.error("Couldn't find member " + user.getDiscordId());
+                continue;
+            }
+
+            if (member.getRoles().contains(Util.getRole("Ultimate Nerd"))
+                    || member.getRoles().contains(Util.getRole("Ultimate Nerd But Red"))
+                    || member.getRoles().contains(Util.getRole("Game Master"))) {
+                log.info("Would have added " + member.getEffectiveName() + " as an inactive user but they have a special role!");
+                continue;
+            }
+
+            stringBuilder.append(" • ").append(member.getUser().getAsMention()).append("\n");
+        }
+
+        log.info("Breakdown by role: " + roles);
+        event.reply(stringBuilder.toString()).setEphemeral(true).queue();
+    }
+
+    /**
+     * returns a view (not a new list) of the sourceList for the
+     * range based on page and pageSize
+     *
+     * @param sourceList
+     * @param page       page number should start from 1
+     * @param pageSize
+     *
+     * @return custom error can be given instead of returning emptyList
+     */
+    public static <T> List<T> getPage(List<T> sourceList, int page, int pageSize) {
+        if (pageSize <= 0 || page <= 0) {
+            throw new IllegalArgumentException("Invalid page: " + pageSize);
+        }
+
+        int fromIndex = (page - 1) * pageSize;
+        if (sourceList == null || sourceList.size() <= fromIndex) {
+            return new ArrayList<>();
+        }
+
+        return sourceList.subList(fromIndex, Math.min(fromIndex + pageSize, sourceList.size()));
+    }
+}
---
+++ src/main/java/net/hypixel/nerdbot/command/ItemGenCommands.java
@@ -0,0 +1,187 @@
+package net.hypixel.nerdbot.command;
+
+import com.freya02.botcommands.api.annotations.Optional;
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.annotations.AppOption;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import com.freya02.botcommands.api.application.slash.autocomplete.AutocompletionMode;
+import com.freya02.botcommands.api.application.slash.autocomplete.annotations.AutocompletionHandler;
+import lombok.extern.log4j.Log4j2;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.hypixel.nerdbot.NerdBotApp;
+import net.hypixel.nerdbot.api.database.user.DiscordUser;
+import net.hypixel.nerdbot.channel.ChannelManager;
+import net.hypixel.nerdbot.generator.MinecraftImage;
+import net.hypixel.nerdbot.generator.StringColorParser;
+import net.hypixel.nerdbot.util.Util;
+import net.hypixel.nerdbot.util.skyblock.MCColor;
+import net.hypixel.nerdbot.util.skyblock.Rarity;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Log4j2
+public class ItemGenCommands extends ApplicationCommand {
+
+    @JDASlashCommand(name = "itemgen", description = "Creates an image that looks like an item from Minecraft, primarily used for Hypixel SkyBlock")
+    public void askForInfo(
+            GuildSlashEvent event,
+            @AppOption(description = "The name of the item") String name,
+            @AppOption(description = "The rarity of the item", autocomplete = "rarities") String rarity,
+            @AppOption(description = "The description of the item") String description,
+            @Optional @AppOption(description = "The type of the item") String type,
+            @Optional @AppOption(description = "If you will handle line breaks at the end of the item's description") Boolean handleLineBreaks,
+            @Optional @AppOption(description = "Sets the background transparency level, 0 for transparent, 255 for opaque") Integer alpha
+    ) throws IOException {
+        String senderChannelId = event.getChannel().getId();
+        String[] itemGenChannelIds = NerdBotApp.getBot().getConfig().getItemGenChannel();
+
+        event.deferReply(false).queue();
+
+        if (itemGenChannelIds == null) {
+            event.getHook().sendMessage("The config for the item generating channel is not ready yet. Try again later!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (Arrays.stream(itemGenChannelIds).noneMatch(senderChannelId::equalsIgnoreCase)) {
+            //The top channel in the config should be considered the 'primary channel', which is referenced in the
+            //error message.
+            TextChannel channel = ChannelManager.getChannel(itemGenChannelIds[0]);
+            if (channel == null) {
+                event.getHook().sendMessage("This can only be used in the item generating channel.").setEphemeral(true).queue();
+                return;
+            }
+            event.getHook().sendMessage("This can only be used in the " + channel.getAsMention() + " channel.").setEphemeral(true).queue();
+            return;
+        }
+
+        // verify rarity argument
+        String finalRarity = rarity;
+        if (Arrays.stream(Rarity.VALUES).noneMatch(rarity1 -> finalRarity.equalsIgnoreCase(rarity1.name()))) {
+            rarity = rarity.replaceAll("[^a-zA-Z0-9_ ]", "");
+            StringBuilder failedRarity = new StringBuilder("You used an invalid rarity, `" + rarity + "`. Valid rarities:\n");
+            Arrays.stream(Rarity.VALUES).forEachOrdered(rarity1 -> failedRarity.append(rarity1.name()).append(" "));
+            event.getHook().sendMessage(failedRarity.toString()).setEphemeral(true).queue();
+            return;
+        }
+
+        StringBuilder itemLore = new StringBuilder(description);
+
+        // adds the item's name to the array list
+        Rarity itemRarity = Rarity.valueOf(rarity.toUpperCase());
+        String createTitle = "%%" + itemRarity.getRarityColor().toString() + "%%" + name + "%%GRAY%%\\n";
+        itemLore.insert(0, createTitle);
+
+        // writing the rarity if the rarity is not none
+        if (itemRarity != Rarity.NONE) {
+            // checks if there is a type for the item
+            if (type == null || type.equalsIgnoreCase("none")) {
+                type = "";
+            }
+            // checking if there is custom line break happening
+            if (handleLineBreaks == null || !handleLineBreaks) {
+                itemLore.append("\\n");
+            }
+
+            // adds the items type in the description
+            String createRarity = "\\n%%" + itemRarity.getRarityColor() + "%%%%BOLD%%" + itemRarity.getId().toUpperCase() + " " + type;
+            itemLore.append(createRarity);
+        }
+
+        // creating a string parser to convert the string into color flagged text
+        StringColorParser colorParser = new StringColorParser();
+        colorParser.parseString(itemLore);
+
+        // checking that there were no errors while parsing the string
+        if (!colorParser.isSuccessfullyParsed()) {
+            event.getHook().sendMessage(colorParser.getErrorString()).setEphemeral(true).queue();
+            return;
+        }
+
+        // checking that the font's have been loaded into memory correctly
+        if (!MinecraftImage.registerFonts()) {
+            event.getHook().sendMessage("It seems that one of the font files couldn't be loaded correctly. Please contact a Bot Developer to have a look at it!").setEphemeral(true).queue();
+            return;
+        }
+
+        // alpha value validation
+        alpha = Objects.requireNonNullElse(alpha, 255); // checks if the image transparency was set
+        alpha = Math.min(255, Math.max(alpha, 0)); // ensure range between 0-254
+
+        MinecraftImage minecraftImage = new MinecraftImage(500, colorParser.getRequiredLines(), MCColor.GRAY, alpha);
+        minecraftImage.drawStrings(colorParser.getParsedDescription());
+        minecraftImage.cropImage();
+        minecraftImage.createImageBorder();
+
+        File imageFile = File.createTempFile("image", ".png");
+        ImageIO.write(minecraftImage.getImage(), "png", imageFile);
+        event.getHook().sendFiles(FileUpload.fromData(imageFile)).setEphemeral(false).queue();
+
+        Member member = event.getMember();
+        DiscordUser discordUser = Util.getOrAddUserToCache(NerdBotApp.getBot().getDatabase(), member.getId());
+        if (discordUser == null) {
+            log.info("Not updating last item generator activity date for " + member.getEffectiveName() + " (ID: " + member.getId() + ") since we cannot find a user!");
+            return;
+        }
+
+        discordUser.getLastActivity().setLastItemGenUsage(System.currentTimeMillis());
+        log.info("Updating last item generator activity date for " + member.getEffectiveName() + " to " + System.currentTimeMillis());
+    }
+
+    @AutocompletionHandler(name = "rarities", mode = AutocompletionMode.CONTINUITY, showUserInput = false)
+    public Queue<String> listRarities(CommandAutoCompleteInteractionEvent event) {
+        return Stream.of(Rarity.VALUES).map(Enum::name).collect(Collectors.toCollection(ArrayDeque::new));
+    }
+
+    @JDASlashCommand(name = "infogen", description = "Get a little bit of help with how to use the Generator bot.")
+    public void askForInfo(GuildSlashEvent event) throws IOException {
+        String senderChannelId = event.getChannel().getId();
+        String[] itemGenChannelIds = NerdBotApp.getBot().getConfig().getItemGenChannel();
+
+        if (itemGenChannelIds == null) {
+            event.reply("The config for the item generating channel is not ready yet. Try again later!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (Arrays.stream(itemGenChannelIds).noneMatch(senderChannelId::equalsIgnoreCase)) {
+            //The top channel in the config should be considered the 'primary channel', which is referenced in the
+            //error message.
+            TextChannel channel = ChannelManager.getChannel(itemGenChannelIds[0]);
+            if (channel == null) {
+                event.reply("This can only be used in the item generating channel.").setEphemeral(true).queue();
+                return;
+            }
+            event.reply("This can only be used in the " + channel.getAsMention() + " channel.").setEphemeral(true).queue();
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Welcome to the Item Generator bot!\n");
+        builder.append("This is a bot used to create custom items to be used in suggestions. You can use the bot with /itemgen, and it accepts a few various arguments:\n\n");
+        builder.append("`name:` The name of the item. Defaults to the rarity color, unless the rarity is none.\n");
+        builder.append("`rarity:` Takes any SkyBlock rarity. Can be left as NONE.\n");
+        builder.append("`description:` Parses a description, including color codes, bold, italics, and newlines.\n");
+        builder.append("`type:` The type of the item, such as a Sword or Wand. Can be left blank.\n");
+        builder.append("`handle_line_breaks- (true/false)`: To be used if you're manually handling line breaks between the description and rarity.\n\n");
+        builder.append("`alpha- (0-255)`: To be used to change the transparency of the background layer. 0 for transparent, 255 for opaque. 245 for overlay.\n\n");
+        builder.append("The Item Generator bot also accepts color codes. You can use these with either manual Minecraft codes, such as `&1`, or `%%DARK_BLUE%%`.\n");
+        builder.append("You can use this same format for stats, such as `%%PRISTINE%%`. \nThis format can also have numbers, where `%%PRISTINE:1%%` will become \"1 ✧ Pristine\"\n");
+        builder.append("Finally, you can move your text to a newline by using \\n. This format can be forced with the handle_line_breaks argument.\n\n");
+        builder.append("Have fun making items! You can click the blue /itemgen command above anyone's image to see what command they're using to create their image. Thanks!\n\n");
+        builder.append("The item generation bot is maintained by mrkeith. Feel free to tag him with any issues.");
+
+
+        event.reply(builder.toString()).setEphemeral(true).queue();
+    }
+}
