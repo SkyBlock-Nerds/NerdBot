e941d0925addc95d45a3312006e02f2fcafd3b23
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-02-22 17:08:22 -0500

    Add helper method for comparing reaction ids to emoji config ids

--- src/main/java/net/hypixel/nerdbot/bot/config/EmojiConfig.java
+++ src/main/java/net/hypixel/nerdbot/bot/config/EmojiConfig.java
@@ -3,6 +3,11 @@ package net.hypixel.nerdbot.bot.config;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import net.dv8tion.jda.api.entities.MessageReaction;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+
+import java.util.Objects;
+import java.util.function.Function;
 
 @Getter
 @Setter
@@ -28,4 +33,11 @@ public class EmojiConfig {
      * The ID of the reaction for the greenlit emoji
      */
     private String greenlitEmojiId;
+
+    public boolean isEquals(MessageReaction reaction, Function<EmojiConfig, String> function) {
+        if (reaction.getEmoji().getType() != Emoji.Type.CUSTOM)
+            return false;
+
+        return Objects.equals(reaction.getEmoji().asCustom().getId(), function.apply(this));
+    }
 }
