b2f184507bf4d565ca4087de9bd9ff799590b991
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-02-22 15:55:15 -0500

    Store the timer to cancel on bot shutdown, support alpha suggestion channels

--- src/main/java/net/hypixel/nerdbot/feature/CurateFeature.java
+++ src/main/java/net/hypixel/nerdbot/feature/CurateFeature.java
@@ -2,6 +2,7 @@ package net.hypixel.nerdbot.feature;
 
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 import net.hypixel.nerdbot.NerdBotApp;
 import net.hypixel.nerdbot.api.curator.Curator;
 import net.hypixel.nerdbot.api.database.Database;
@@ -9,52 +10,68 @@ import net.hypixel.nerdbot.api.database.greenlit.GreenlitMessage;
 import net.hypixel.nerdbot.api.feature.BotFeature;
 import net.hypixel.nerdbot.curator.ForumChannelCurator;
 
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
+import java.util.stream.Stream;
 
 @Log4j2
 public class CurateFeature extends BotFeature {
 
+    private final Timer timer = new Timer();
+
     @Override
     public void onStart() {
-        if (NerdBotApp.getBot().getConfig().getSuggestionForumId() == null) {
-            log.info("Not starting CurateFeature as 'suggestionForumId' could not be found in the configuration file!");
+        if (NerdBotApp.getBot().getConfig().getSuggestionForumIds() == null) {
+            log.info("Not starting CurateFeature as 'suggestionForumIds' could not be found in the configuration file!");
             return;
         }
 
-        Database database = NerdBotApp.getBot().getDatabase();
-        TimerTask timerTask = new TimerTask() {
+        this.timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                Curator<ForumChannel> forumChannelCurator = new ForumChannelCurator(NerdBotApp.getBot().isReadOnly());
-                ForumChannel forumChannel = NerdBotApp.getBot().getJDA().getForumChannelById(NerdBotApp.getBot().getConfig().getSuggestionForumId());
-                if (forumChannel == null) {
-                    log.error("Couldn't find the suggestion forum channel from the bot config!");
-                    return;
-                }
-
                 NerdBotApp.EXECUTOR_SERVICE.execute(() -> {
-                    List<GreenlitMessage> result = forumChannelCurator.curate(forumChannel);
-                    if (result.isEmpty()) {
-                        log.info("No new suggestions were greenlit this time!");
-                    } else {
-                        log.info("Greenlit " + result.size() + " new suggestions in " + (forumChannelCurator.getEndTime() - forumChannelCurator.getStartTime()) + "ms!");
-                    }
-                    result.forEach(greenlitMessage -> {
-                        database.upsertDocument(database.getCollection("greenlit_messages", GreenlitMessage.class), "messageId", greenlitMessage.getMessageId(), greenlitMessage);
+                    Database database = NerdBotApp.getBot().getDatabase();
+                    final Curator<ForumChannel> forumChannelCurator = new ForumChannelCurator(NerdBotApp.getBot().isReadOnly());
+
+                    Stream.concat(
+                            Arrays.stream(NerdBotApp.getBot().getConfig().getSuggestionForumIds()).map(forumId -> Pair.of(forumId, false)),
+                            Arrays.stream(NerdBotApp.getBot().getConfig().getAlphaSuggestionForumIds()).map(forumId -> Pair.of(forumId, true))
+                    ).forEach(suggestionForum -> {
+                        String id = suggestionForum.getLeft();
+                        boolean alpha = suggestionForum.getRight();
+                        ForumChannel forumChannel = NerdBotApp.getBot().getJDA().getForumChannelById(id);
+                        log.info("Processing" + (alpha ? " alpha" : "") + " suggestion forum channel with ID " + id + ".");
+
+                        if (forumChannel == null) {
+                            log.error("Couldn't find the suggestion forum channel with ID " + id + "!");
+                            return;
+                        }
+
+                        List<GreenlitMessage> result = forumChannelCurator.curate(forumChannel);
+                        if (result.isEmpty()) {
+                            log.info("No new suggestions were greenlit from ID " + id + " this time!");
+                        } else {
+                            log.info("Greenlit " + result.size() + " new suggestions from ID " + id + ". Took " + (forumChannelCurator.getEndTime() - forumChannelCurator.getStartTime()) + "ms!");
+                        }
+
+                        // Update Database
+                        result.forEach(greenlitMessage -> database.upsertDocument(
+                                database.getCollection(
+                                        "greenlit_messages",
+                                        GreenlitMessage.class
+                                ),
+                                "messageId",
+                                greenlitMessage.getMessageId(),
+                                greenlitMessage
+                        ));
+                        log.info("Inserted " + result.size() + " new greenlit messages into the database!");
                     });
-                    log.info("Inserted " + result.size() + " new greenlit messages into the database!");
                 });
             }
-        };
-
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(timerTask, 30_000L, NerdBotApp.getBot().getConfig().getInterval());
+        }, 30_000L, NerdBotApp.getBot().getConfig().getInterval());
     }
 
     @Override
     public void onEnd() {
-
+        this.timer.cancel();
     }
 }
