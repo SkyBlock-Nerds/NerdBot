c313be6e9f7b2e91d474289468fa8d9cd7a891e2
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-02-22 14:45:22 -0500

    Handle alpha vc activity

--- src/main/java/net/hypixel/nerdbot/listener/ActivityListener.java
+++ src/main/java/net/hypixel/nerdbot/listener/ActivityListener.java
@@ -2,6 +2,7 @@ package net.hypixel.nerdbot.listener;
 
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
@@ -32,25 +33,22 @@ public class ActivityListener {
 
     @SubscribeEvent
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.isFromGuild()) {
-            return;
-        }
+        if (!event.isFromGuild())
+            return; // Ignore Non Guild
 
         Member member = event.getMember();
-        GuildChannel channel = event.getGuildChannel();
-
-        if (!event.isFromGuild() || member == null || member.getUser().isBot()) {
+        if (member == null || member.getUser().isBot()) {
             return;
         }
 
         DiscordUser discordUser = Util.getOrAddUserToCache(database, member.getId());
+
+        if (discordUser == null)
+            return; // Ignore Empty User
+
+        GuildChannel channel = event.getGuildChannel();
         long time = System.currentTimeMillis();
-
-        if (discordUser == null) {
-            return;
-        }
-
-        if (channel.getName().contains("alpha")) {
+        if (channel.getName().toLowerCase().contains("alpha")) {
             discordUser.getLastActivity().setLastAlphaActivity(time);
             log.info("Updating last alpha activity date for " + member.getEffectiveName() + " to " + time);
         }
@@ -63,15 +61,23 @@ public class ActivityListener {
     public void onVoiceChannelJoin(GuildVoiceUpdateEvent event) {
         Member member = event.getMember();
 
-        if (member.getUser().isBot()) {
-            return;
-        }
+        if (member.getUser().isBot())
+            return; // Ignore Bots
 
         DiscordUser discordUser = Util.getOrAddUserToCache(database, member.getId());
-        if (discordUser == null) {
-            return;
+        if (discordUser == null)
+            return; // Ignore Empty User
+
+        long time = System.currentTimeMillis();
+        if (event.getChannelJoined() instanceof VoiceChannel) {
+            VoiceChannel channel = event.getChannelJoined().asVoiceChannel();
+
+            if (channel.getName().toLowerCase().contains("alpha")) {
+                discordUser.getLastActivity().setAlphaVoiceJoinDate(time);
+                log.info("Updating last alpha voice activity date for " + member.getEffectiveName() + " to " + time);
+            }
         }
-        discordUser.getLastActivity().setLastVoiceChannelJoinDate(System.currentTimeMillis());
-        log.info("Updating last voice channel activity date for " + member.getEffectiveName() + " to " + System.currentTimeMillis());
+        discordUser.getLastActivity().setLastVoiceChannelJoinDate(time);
+        log.info("Updating last global voice activity date for " + member.getEffectiveName() + " to " + time);
     }
 }
