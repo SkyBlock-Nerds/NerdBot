9f080758ca023b93944f18930deb48ea0db2dd55
Brian <6407029+Nahydrin@users.noreply.github.com>
2023-02-22 19:29:44 -0500

    Separate Suggestion and Alpha User Activity (#61)
    * Separate suggestion activity into creation, vote and join dates between global and alpha
    
    * New info response for user activity using new data
    
    * Add alpha tag to greenlit message
    
    * Handle alpha vc activity
    
    * Fix conflict
    
    * Remove todos, add to pull request
    
    * Store multiple suggestion forum ids, add alpha suggestion forum ids
    
    * Store the timer to cancel on bot shutdown, support alpha suggestion channels
    
    * Better reaction stream
    
    * Add helper method for comparing reaction ids to emoji config ids
    
    * Implement alpha suggestion create/vote/comment, suggestion vote/comment
    
    * Store timer in BotFeature
    
    * Fix forum suggestion searching
    
    * Add braces reeee
    
    * Remove empty line

--- src/main/java/net/hypixel/nerdbot/feature/GreenlitUpdateFeature.java
+++ src/main/java/net/hypixel/nerdbot/feature/GreenlitUpdateFeature.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.entities.channel.forums.BaseForumTag;
 import net.dv8tion.jda.api.entities.channel.forums.ForumTag;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 import net.hypixel.nerdbot.NerdBotApp;
 import net.hypixel.nerdbot.api.bot.Bot;
 import net.hypixel.nerdbot.api.database.Database;
@@ -16,111 +17,114 @@ import net.hypixel.nerdbot.api.database.greenlit.GreenlitMessage;
 import net.hypixel.nerdbot.api.feature.BotFeature;
 import net.hypixel.nerdbot.bot.config.EmojiConfig;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 @Log4j2
 public class GreenlitUpdateFeature extends BotFeature {
 
     @Override
     public void onStart() {
-        TimerTask timerTask = new TimerTask() {
+        this.timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                Bot nerdBot = NerdBotApp.getBot();
-                ForumChannel suggestions = nerdBot.getJDA().getForumChannelById(nerdBot.getConfig().getSuggestionForumId());
-                if (suggestions == null) {
-                    log.error("Couldn't find the suggestion forum channel from the bot config!");
-                    return;
-                }
+                Stream.concat(
+                        Arrays.stream(NerdBotApp.getBot().getConfig().getSuggestionForumIds()).map(forumId -> Pair.of(forumId, false)),
+                        Arrays.stream(NerdBotApp.getBot().getConfig().getAlphaSuggestionForumIds()).map(forumId -> Pair.of(forumId, true))
+                ).forEach(suggestionForum -> {
+                    String id = suggestionForum.getLeft();
+                    boolean alpha = suggestionForum.getRight();
+                    ForumChannel forumChannel = NerdBotApp.getBot().getJDA().getForumChannelById(id);
+                    log.info("Processing" + (alpha ? " alpha" : "") + " suggestion forum channel with ID " + id + ".");
 
-                List<ThreadChannel> greenlitThreads = new ArrayList<>(suggestions.getThreadChannels().stream().filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).toList().contains("Greenlit")).toList());
-                List<ThreadChannel> archived = suggestions.retrieveArchivedPublicThreadChannels().complete();
-
-                log.info("Found " + suggestions.getThreadChannels().size() + " threads in the suggestion forum channel!");
-                log.info("Found " + archived.size() + " archived threads in the suggestion forum channel!");
-
-                greenlitThreads.addAll(archived.stream()
-                        .filter(threadChannel -> !greenlitThreads.contains(threadChannel))
-                        .filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).toList().contains("Greenlit"))
-                        .toList());
-
-                Database database = nerdBot.getDatabase();
-                if (!database.isConnected()) {
-                    log.info("Database is not connected, skipping greenlit message update!");
-                    return;
-                }
-
-                List<GreenlitMessage> greenlits = database.getCollection("greenlit_messages", GreenlitMessage.class).find().into(new ArrayList<>());
-                if (greenlits.size() == 0) {
-                    log.info("No greenlit messages found in the database to update!");
-                    return;
-                }
-
-                log.info("Found " + greenlitThreads.size() + " total greenlit threads");
-                log.info("Found " + greenlits.size() + " greenlit messages in the database!");
-
-                greenlits.forEach(greenlitMessage -> {
-                    if (greenlitThreads.stream().anyMatch(threadChannel -> threadChannel.getId().equals(greenlitMessage.getMessageId()))) {
-                        ThreadChannel thread = greenlitThreads.stream().filter(threadChannel -> threadChannel.getId().equals(greenlitMessage.getMessageId())).findFirst().orElse(null);
-                        if (thread == null) {
-                            return;
-                        }
-
-                        log.info("Found matching thread for greenlit message " + greenlitMessage.getMessageId() + ": '" + thread.getName() + "' (ID: " + thread.getId() + ")");
-
-                        MessageHistory history = thread.getHistoryFromBeginning(1).complete();
-                        Message message = history.getRetrievedHistory().get(0);
-                        if (message == null) {
-                            log.error("Message for thread '" + thread.getName() + "' (ID: " + thread.getId() + ") is null!");
-                            return;
-                        }
-
-                        EmojiConfig emojiConfig = nerdBot.getConfig().getEmojiConfig();
-                        List<MessageReaction> reactions = message.getReactions()
-                                .stream()
-                                .filter(reaction -> reaction.getEmoji().getType() == Emoji.Type.CUSTOM)
-                                .toList();
-                        int agree = reactions.stream()
-                                .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getAgreeEmojiId()))
-                                .mapToInt(MessageReaction::getCount)
-                                .findFirst()
-                                .orElse(0);
-                        int disagree = reactions.stream()
-                                .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getDisagreeEmojiId()))
-                                .mapToInt(MessageReaction::getCount)
-                                .findFirst()
-                                .orElse(0);
-                        int neutral = reactions.stream()
-                                .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getNeutralEmojiId()))
-                                .mapToInt(MessageReaction::getCount)
-                                .findFirst()
-                                .orElse(0);
-
-                        greenlitMessage.setSuggestionTitle(thread.getName());
-                        greenlitMessage.setSuggestionContent(message.getContentRaw());
-                        greenlitMessage.setTags(thread.getAppliedTags().stream().map(BaseForumTag::getName).toList());
-                        greenlitMessage.setAgrees(agree);
-                        greenlitMessage.setNeutrals(neutral);
-                        greenlitMessage.setDisagrees(disagree);
-                        greenlitMessage.setNeutrals(neutral);
-
-                        database.upsertDocument(database.getCollection("greenlit_messages", GreenlitMessage.class), "messageId", greenlitMessage.getMessageId(), greenlitMessage);
-                        log.info("Updated greenlit message " + greenlitMessage.getMessageId() + " in the database!");
+                    if (forumChannel == null) {
+                        log.error("Couldn't find the suggestion forum channel with ID " + id + "!");
+                        return;
                     }
+
+                    List<ThreadChannel> greenlitThreads = new ArrayList<>(forumChannel.getThreadChannels().stream().filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).toList().contains("Greenlit")).toList());
+                    List<ThreadChannel> archived = forumChannel.retrieveArchivedPublicThreadChannels().complete();
+
+                    log.info("Found " + forumChannel.getThreadChannels().size() + " threads in the suggestion forum channel!");
+                    log.info("Found " + archived.size() + " archived threads in the suggestion forum channel!");
+
+                    greenlitThreads.addAll(archived.stream()
+                            .filter(threadChannel -> !greenlitThreads.contains(threadChannel))
+                            .filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).toList().contains("Greenlit"))
+                            .toList());
+
+                    Database database = NerdBotApp.getBot().getDatabase();
+                    if (!database.isConnected()) {
+                        log.info("Database is not connected, skipping greenlit message update!");
+                        return;
+                    }
+
+                    List<GreenlitMessage> greenlits = database.getCollection("greenlit_messages", GreenlitMessage.class).find().into(new ArrayList<>());
+                    if (greenlits.size() == 0) {
+                        log.info("No greenlit messages found in the database to update!");
+                        return;
+                    }
+
+                    log.info("Found " + greenlitThreads.size() + " total greenlit threads");
+                    log.info("Found " + greenlits.size() + " greenlit messages in the database!");
+
+                    greenlits.forEach(greenlitMessage -> {
+                        if (greenlitThreads.stream().anyMatch(threadChannel -> threadChannel.getId().equals(greenlitMessage.getMessageId()))) {
+                            ThreadChannel thread = greenlitThreads.stream().filter(threadChannel -> threadChannel.getId().equals(greenlitMessage.getMessageId())).findFirst().orElse(null);
+                            if (thread == null) {
+                                return;
+                            }
+
+                            log.info("Found matching thread for greenlit message " + greenlitMessage.getMessageId() + ": '" + thread.getName() + "' (ID: " + thread.getId() + ")");
+
+                            MessageHistory history = thread.getHistoryFromBeginning(1).complete();
+                            Message message = history.getRetrievedHistory().get(0);
+                            if (message == null) {
+                                log.error("Message for thread '" + thread.getName() + "' (ID: " + thread.getId() + ") is null!");
+                                return;
+                            }
+
+                            EmojiConfig emojiConfig = NerdBotApp.getBot().getConfig().getEmojiConfig();
+                            List<MessageReaction> reactions = message.getReactions()
+                                    .stream()
+                                    .filter(reaction -> reaction.getEmoji().getType() == Emoji.Type.CUSTOM)
+                                    .toList();
+                            int agree = reactions.stream()
+                                    .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getAgreeEmojiId()))
+                                    .mapToInt(MessageReaction::getCount)
+                                    .findFirst()
+                                    .orElse(0);
+                            int disagree = reactions.stream()
+                                    .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getDisagreeEmojiId()))
+                                    .mapToInt(MessageReaction::getCount)
+                                    .findFirst()
+                                    .orElse(0);
+                            int neutral = reactions.stream()
+                                    .filter(reaction -> reaction.getEmoji().asCustom().getId().equalsIgnoreCase(emojiConfig.getNeutralEmojiId()))
+                                    .mapToInt(MessageReaction::getCount)
+                                    .findFirst()
+                                    .orElse(0);
+
+                            greenlitMessage.setSuggestionTitle(thread.getName());
+                            greenlitMessage.setSuggestionContent(message.getContentRaw());
+                            greenlitMessage.setTags(thread.getAppliedTags().stream().map(BaseForumTag::getName).toList());
+                            greenlitMessage.setAgrees(agree);
+                            greenlitMessage.setNeutrals(neutral);
+                            greenlitMessage.setDisagrees(disagree);
+                            greenlitMessage.setNeutrals(neutral);
+
+                            database.upsertDocument(database.getCollection("greenlit_messages", GreenlitMessage.class), "messageId", greenlitMessage.getMessageId(), greenlitMessage);
+                            log.info("Updated greenlit message " + greenlitMessage.getMessageId() + " in the database!");
+                        }
+                    });
                 });
             }
-        };
-
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(timerTask, 0L, TimeUnit.HOURS.toMillis(1L));
+        }, 0L, TimeUnit.HOURS.toMillis(1L));
     }
 
     @Override
     public void onEnd() {
-
+        this.timer.cancel();
     }
 }
