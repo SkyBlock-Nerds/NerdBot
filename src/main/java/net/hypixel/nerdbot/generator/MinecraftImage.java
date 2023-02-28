39973674d0a48eb63586aa72f95a96286c4ca35d
Brian <6407029+Nahydrin@users.noreply.github.com>
2023-02-28 14:17:43 -0500

    Added Strikethrough/Underlined Format and Padding Support to Item Generator (#65)
    * Rewrite class to support Strikethrough/Underlined, better font registration, better method naming
    
    * Add handling for strikethrough and underlined codes
    
    * Add strikethrough and underlined codes, rgb does not matter here
    
    * Add strikethrough and underlined variables
    
    * Add optional padding parameter to command, update MinecraftImage creation
    
    * Add padding to infogen, fix formatting
    
    * Aerh is bald

--- src/main/java/net/hypixel/nerdbot/generator/MinecraftImage.java
+++ src/main/java/net/hypixel/nerdbot/generator/MinecraftImage.java
@@ -1,181 +1,252 @@
 package net.hypixel.nerdbot.generator;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import net.hypixel.nerdbot.command.ItemGenCommands;
 import net.hypixel.nerdbot.util.skyblock.MCColor;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.UUID;
 
 public class MinecraftImage {
+
     private static final int PIXEL_SIZE = 2;
     private static final int START_XY = PIXEL_SIZE * 5;
     private static final int Y_INCREMENT = PIXEL_SIZE * 10;
-
-    private final GraphicsEnvironment ge;
-    private final Graphics2D g2d;
+    private static final int STRIKETHROUGH_OFFSET = -8;
+    private static final int UNDERLINE_OFFSET = 2;
     private static boolean fontsRegistered = false;
-    private static final Font[] minecraftFonts = new Font[4];
+    private static final Font[] minecraftFonts;
     private static Font sansSerif = null;
 
-    private MCColor currentColor;
+    // Current Settings
+    @Getter private final ArrayList<ArrayList<ColoredString>> lines;
+    @Getter private final int alpha;
+    @Getter private final int padding;
+    @Getter(AccessLevel.PRIVATE)
+    private final Graphics2D graphics;
+    @Getter private BufferedImage image;
+    @Getter private MCColor currentColor;
     private Font currentFont;
-    private BufferedImage image;
+
+    // Positioning & Size
     private int locationX = START_XY;
     private int locationY = START_XY + PIXEL_SIZE * 2 + Y_INCREMENT / 2;
     private int largestWidth = 0;
-    private final int alphaValue;
 
-    public MinecraftImage(int imageWidth, int linesToPrint, MCColor defaultColor, int alpha) {
-        this.ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    static {
+        sansSerif = new Font("SansSerif", Font.PLAIN, 20);
+        minecraftFonts = new Font[] {
+            initFont("/Minecraft/minecraft.otf", 15.5f),
+            initFont("/Minecraft/3_Minecraft-Bold.otf", 20.0f),
+            initFont("/Minecraft/2_Minecraft-Italic.otf", 20.5f),
+            initFont("/Minecraft/4_Minecraft-BoldItalic.otf", 20.5f)
+        };
 
-        // registers the static fonts
-        for (Font font : minecraftFonts) {
-            ge.registerFont(font);
-        }
+        // Register Minecraft Fonts
+        Arrays.stream(minecraftFonts).forEach(GraphicsEnvironment.getLocalGraphicsEnvironment()::registerFont);
+    }
 
-        this.alphaValue = alpha;
-        this.g2d = initG2D(imageWidth, linesToPrint * Y_INCREMENT + START_XY + PIXEL_SIZE * 4);
+    public MinecraftImage(ArrayList<ArrayList<ColoredString>> lines, MCColor defaultColor, int defaultWidth, int alpha, int padding) {
+        this.alpha = alpha;
+        this.padding = padding;
+        this.lines = lines;
+        this.graphics = this.initG2D(defaultWidth, this.lines.size() * Y_INCREMENT + START_XY + PIXEL_SIZE * 4);
         this.currentColor = defaultColor;
     }
 
-    public BufferedImage getImage() {
-        return this.image;
-    }
-
     /**
-     * Creates an image, then initialized a Graphics2D object from that image.
+     * Creates an image, then initializes a Graphics2D object from that image.
      *
      * @return G2D object
      */
     private Graphics2D initG2D(int width, int height) {
+        // Create Image
         this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-        Graphics2D graphics = image.createGraphics();
-
-        // drawing main background
-        graphics.setColor(new Color(18, 3, 18, alphaValue));
-        graphics.fillRect(PIXEL_SIZE * 2, PIXEL_SIZE * 2, width - PIXEL_SIZE * 4, height - PIXEL_SIZE * 4);
+        // Draw Primary Background
+        Graphics2D graphics = this.getImage().createGraphics();
+        graphics.setColor(new Color(18, 3, 18, this.getAlpha()));
+        graphics.fillRect(
+            PIXEL_SIZE * 2,
+            PIXEL_SIZE * 2,
+            width - PIXEL_SIZE * 4,
+            height - PIXEL_SIZE * 4
+        );
 
         return graphics;
     }
 
     /**
-     * Creates the purple border (and border around that) around the image
+     * Crops the image to fit the space taken up by the borders.
      */
-    public void createImageBorder() {
-        final int width = image.getWidth();
-        final int height = image.getHeight();
-
-        // draws darker purple border around purple rectangle
-        g2d.setColor(new Color(18, 3, 18, alphaValue));
-        g2d.fillRect(0, PIXEL_SIZE, PIXEL_SIZE, height - PIXEL_SIZE * 2);
-        g2d.fillRect(PIXEL_SIZE, 0, width - PIXEL_SIZE * 2, PIXEL_SIZE);
-        g2d.fillRect(width - PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, height - PIXEL_SIZE * 2);
-        g2d.fillRect(PIXEL_SIZE, height - PIXEL_SIZE, width - PIXEL_SIZE * 2, PIXEL_SIZE);
-
-        // drawing the purple rectangle around the edge
-        g2d.setColor(new Color(37, 0, 94, alphaValue));
-        g2d.drawRect(PIXEL_SIZE, PIXEL_SIZE, width - PIXEL_SIZE * 2 - 1, height - PIXEL_SIZE * 2 - 1);
-        g2d.drawRect(PIXEL_SIZE + 1, PIXEL_SIZE + 1, width - PIXEL_SIZE * 3 - 1, height - PIXEL_SIZE * 3 - 1);
+    public void cropImage() {
+        this.image = this.getImage().getSubimage(
+            0,
+            0,
+            this.largestWidth + START_XY,
+            this.getImage().getHeight()
+        );
     }
 
     /**
-     * Crops the image down to closely fit the size taken up
+     * Resizes the image to add padding.
      */
-    public void cropImage() {
-        image = image.getSubimage(0, 0, largestWidth + START_XY, image.getHeight());
+    public void addPadding() {
+        if (this.getPadding() > 0) {
+            BufferedImage resizedImage = new BufferedImage(
+                this.getImage().getWidth() + this.getPadding() * 2,
+                this.getImage().getHeight() + this.getPadding() * 2,
+                BufferedImage.TYPE_INT_ARGB
+            );
+
+            Graphics2D graphics2D = resizedImage.createGraphics();
+            graphics2D.drawImage(this.getImage(), this.getPadding(), this.getPadding(), this.getImage().getWidth(), this.getImage().getHeight(), null);
+            graphics2D.dispose();
+            this.image = resizedImage;
+        }
+    }
+
+    /**
+     * Creates the inner and outer purple borders around the image.
+     */
+    public void drawBorders() {
+        final int width = this.getImage().getWidth();
+        final int height = this.getImage().getHeight();
+
+        // Draw Darker Purple Border Around Purple Border
+        this.getGraphics().setColor(new Color(18, 3, 18, this.getAlpha()));
+        this.getGraphics().fillRect(0, PIXEL_SIZE, PIXEL_SIZE, height - PIXEL_SIZE * 2);
+        this.getGraphics().fillRect(PIXEL_SIZE, 0, width - PIXEL_SIZE * 2, PIXEL_SIZE);
+        this.getGraphics().fillRect(width - PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, height - PIXEL_SIZE * 2);
+        this.getGraphics().fillRect(PIXEL_SIZE, height - PIXEL_SIZE, width - PIXEL_SIZE * 2, PIXEL_SIZE);
+
+        // Draw Purple Border
+        this.getGraphics().setColor(new Color(37, 0, 94, this.getAlpha()));
+        this.getGraphics().drawRect(PIXEL_SIZE, PIXEL_SIZE, width - PIXEL_SIZE * 2 - 1, height - PIXEL_SIZE * 2 - 1);
+        this.getGraphics().drawRect(PIXEL_SIZE + 1, PIXEL_SIZE + 1, width - PIXEL_SIZE * 3 - 1, height - PIXEL_SIZE * 3 - 1);
     }
 
     /**
      * Draws the strings to the generated image.
-     *
-     * @param parsedString A string with valid color codes, such as through #parseDescription().
      */
-    public void drawStrings(ArrayList<ArrayList<ColoredString>> parsedString) {
-        for (ArrayList<ColoredString> line : parsedString) {
-            for (ColoredString colorSegment : line) {
+    public void drawLines() {
+        for (ArrayList<ColoredString> line : this.getLines()) {
+            for (ColoredString segment : line) {
                 // setting the font if it is meant to be bold or italicised
-                currentFont = minecraftFonts[(colorSegment.isBold() ? 1 : 0) + (colorSegment.isItalic() ? 2 : 0)];
-                g2d.setFont(currentFont);
-
-                currentColor = colorSegment.getCurrentColor();
+                currentFont = minecraftFonts[(segment.isBold() ? 1 : 0) + (segment.isItalic() ? 2 : 0)];
+                this.getGraphics().setFont(currentFont);
+                currentColor = segment.getCurrentColor();
 
                 StringBuilder subWord = new StringBuilder();
-                String displayingLine = colorSegment.toString();
+                String displayingLine = segment.toString();
 
                 // iterating through all the indexes of the current line until there is a character which cannot be displayed
                 for (int charIndex = 0; charIndex < displayingLine.length(); charIndex++) {
                     char character = displayingLine.charAt(charIndex);
+
                     if (!currentFont.canDisplay(character)) {
-                        drawStringWithBackground(subWord.toString());
+                        this.drawString(subWord.toString(), segment);
                         subWord.setLength(0);
-                        drawSymbol(character);
+                        this.drawSymbol(character, segment);
                         continue;
                     }
-                    // We do this to prevent monospace bullshit
+
+                    // Prevent Monospace
                     subWord.append(character);
                 }
 
-                drawStringWithBackground(subWord.toString());
+                this.drawString(subWord.toString(), segment);
             }
-            newLine(parsedString.indexOf(line) == 0);
+
+            this.updatePositionAndSize(this.getLines().indexOf(line) == 0);
         }
     }
 
     /**
-     * Draws a symbol on the image, and moves the locationX to where needed.
+     * Draws a symbol on the image, and updates the pointer location.
      *
-     * @param symbol Symbol to be drawn.
+     * @param symbol The symbol to draw.
      */
-    private void drawSymbol(char symbol) {
-        g2d.setFont(sansSerif);
-
-        g2d.setColor(currentColor.getBackgroundColor());
-        g2d.drawString(Character.toString(symbol), locationX + 2, locationY + 2);
-
-        g2d.setColor(currentColor.getColor());
-        g2d.drawString(Character.toString(symbol), locationX, locationY);
-
-        locationX += sansSerif.getStringBounds(Character.toString(symbol), g2d.getFontRenderContext()).getWidth();
-        g2d.setFont(currentFont);
+    private void drawSymbol(char symbol, @NotNull ColoredString segment) {
+        this.drawString(Character.toString(symbol), segment, sansSerif);
     }
 
     /**
-     * Draws a string at the current location, and moves the locationX to where needed.
+     * Draws a string at the current location, and updates the pointer location.
      *
-     * @param string String to print.
+     * @param value The value to draw.
      */
-    private void drawStringWithBackground(String string) {
-        // drawing the drop shadow text
-        g2d.setColor(currentColor.getBackgroundColor());
-        g2d.drawString(string, locationX + 2, locationY + 2);
-        // drawing the normal text
-        g2d.setColor(currentColor.getColor());
-        g2d.drawString(string, locationX, locationY);
-
-        // Move the printing location depending on the size of the printed string
-        locationX += currentFont.getStringBounds(string, g2d.getFontRenderContext()).getWidth();
+    private void drawString(@NotNull String value, @NotNull ColoredString segment) {
+        this.drawString(value, segment, this.currentFont);
     }
 
-    /**
-     * Moves the string to print to the next line.
-     *
-     * @param increaseGap - Increase number of pixels between next line
-     */
-    private void newLine(boolean increaseGap) {
-        locationY += Y_INCREMENT;
-        if (increaseGap) {
-            locationY += PIXEL_SIZE * 2;
+    private void drawString(@NotNull String value, @NotNull ColoredString segment, @NotNull Font font) {
+        // Change Font
+        this.getGraphics().setFont(font);
+
+        // Next Draw Position
+        int nextBounds = (int) font.getStringBounds(value, this.getGraphics().getFontRenderContext()).getWidth();
+
+        // Draw Strikethrough Drop Shadow
+        if (segment.isStrikethrough()) {
+            this.drawThickLine(nextBounds, this.locationX, this.locationY, -1, STRIKETHROUGH_OFFSET, true);
         }
 
-        if (locationX > largestWidth) {
-            largestWidth = locationX;
+        // Draw Underlined Drop Shadow
+        if (segment.isUnderlined()) {
+            this.drawThickLine(nextBounds, this.locationX - PIXEL_SIZE, this.locationY, 1, UNDERLINE_OFFSET, true);
         }
-        locationX = START_XY;
+
+        // Draw Drop Shadow Text
+        this.getGraphics().setColor(this.currentColor.getBackgroundColor());
+        this.getGraphics().drawString(value, this.locationX + PIXEL_SIZE, this.locationY + PIXEL_SIZE);
+
+        // Draw Text
+        this.getGraphics().setColor(this.currentColor.getColor());
+        this.getGraphics().drawString(value, this.locationX, this.locationY);
+
+        // Draw Strikethrough
+        if (segment.isStrikethrough()) {
+            this.drawThickLine(nextBounds, this.locationX, this.locationY, -1, STRIKETHROUGH_OFFSET, false);
+        }
+
+        // Draw Underlined
+        if (segment.isUnderlined()) {
+            this.drawThickLine(nextBounds, this.locationX - PIXEL_SIZE, this.locationY, 1, UNDERLINE_OFFSET, false);
+        }
+
+        // Update Draw Pointer Location
+        this.locationX += nextBounds;
+
+        // Reset Font
+        this.getGraphics().setFont(this.currentFont);
+    }
+
+    private void drawThickLine(int width, int xPosition, int yPosition, int xOffset, int yOffset, boolean dropShadow) {
+        int xPosition1 = xPosition;
+        int xPosition2 = xPosition + width + xOffset;
+        yPosition += yOffset;
+
+        if (dropShadow) {
+            xPosition1 += PIXEL_SIZE;
+            xPosition2 += PIXEL_SIZE;
+            yPosition += PIXEL_SIZE;
+        }
+
+        this.getGraphics().setColor(dropShadow ? this.currentColor.getBackgroundColor() : this.currentColor.getColor());
+        this.getGraphics().drawLine(xPosition1, yPosition, xPosition2, yPosition);
+        this.getGraphics().drawLine(xPosition1, yPosition + 1, xPosition2, yPosition + 1);
     }
 
     /**
@@ -201,32 +272,36 @@ public class MinecraftImage {
         return font;
     }
 
-    /**
-     * Initializes all required fonts before executing
-     *
-     * @return If registering fonts was successful
-     */
-    public static boolean registerFonts() {
-        if (fontsRegistered)
-            return true;
-
-        if (minecraftFonts[0] == null) {
-            minecraftFonts[0] = initFont("/Minecraft/minecraft.otf", 15.5f);
-        }
-        if (minecraftFonts[1] == null) {
-            minecraftFonts[1] = initFont("/Minecraft/3_Minecraft-Bold.otf", 20.0f);
-        }
-        if (minecraftFonts[2] == null) {
-            minecraftFonts[2] = initFont("/Minecraft/2_Minecraft-Italic.otf", 20.5f);
-        }
-        if (minecraftFonts[3] == null) {
-            minecraftFonts[3] = initFont("/Minecraft/4_Minecraft-BoldItalic.otf", 20.5f);
-        }
-        if (sansSerif == null) {
-            sansSerif = new Font("SansSerif", Font.PLAIN, 20);
-        }
-
-        fontsRegistered = minecraftFonts[0] != null && minecraftFonts[1] != null && minecraftFonts[2] != null && minecraftFonts[3] != null;
-        return fontsRegistered;
+    public static boolean isFontsRegistered() {
+        return Arrays.stream(minecraftFonts).noneMatch(Objects::isNull);
     }
+
+    /**
+     * Draws the Lines, Resizes the Image and Draws the Borders.
+     */
+    public MinecraftImage render() {
+        this.drawLines();
+        this.cropImage();
+        this.drawBorders();
+        this.addPadding();
+        return this;
+    }
+
+    public File toFile() throws IOException {
+        File tempFile = File.createTempFile("image", ".png");
+        ImageIO.write(this.getImage(), "PNG", tempFile);
+        return tempFile;
+    }
+
+    /**
+     * Moves the pointer to draw on the next line.
+     *
+     * @param increaseGap Increase number of pixels between the next line
+     */
+    private void updatePositionAndSize(boolean increaseGap) {
+        this.locationY += Y_INCREMENT + (increaseGap ? PIXEL_SIZE * 2 : 0);
+        this.largestWidth = Math.max(this.locationX, this.largestWidth);
+        this.locationX = START_XY;
+    }
+
 }
