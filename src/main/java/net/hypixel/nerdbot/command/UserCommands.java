7149952f0950311d58a85fc4d82d83f0226cca21
Aerhhh <callum@aerh.net>
2023-06-19 23:31:13 +0100

    Keith wants comments

--- src/main/java/net/hypixel/nerdbot/command/UserCommands.java
+++ src/main/java/net/hypixel/nerdbot/command/UserCommands.java
@@ -33,6 +33,7 @@ public class UserCommands extends ApplicationCommand {
         Date date = null;
         boolean parsed = false;
 
+        // Try parse the time using the Natty dependency
         try {
             Parser parser = new Parser();
             List<DateGroup> groups = parser.parse(time);
@@ -42,25 +43,30 @@ public class UserCommands extends ApplicationCommand {
             date = dates.get(0);
             parsed = true;
         } catch (IndexOutOfBoundsException ignored) {
+            // Ignore this exception, it means that the provided time was not parsed
         }
 
+        // Try parse the time using the regex if the above failed
         if (!parsed) {
             try {
                 date = parseCustomFormat(time);
-            } catch (DateTimeParseException exception2) {
+            } catch (DateTimeParseException exception) {
                 event.reply("Could not parse the provided time!").setEphemeral(true).queue();
                 return;
             }
         }
 
+        // Check if the provided time is in the past
         if (date.before(new Date())) {
             event.reply("You can't set a reminder in the past!").setEphemeral(true).queue();
             return;
         }
 
+        // Create a new reminder and save it to the database
         Reminder reminder = new Reminder(description, date, event.getChannel().getId(), event.getUser().getId());
         InsertOneResult result = reminder.save();
 
+        // Check if the reminder was saved successfully, schedule it and send a confirmation message
         if (result != null && result.wasAcknowledged() && result.getInsertedId() != null) {
             event.reply("I will remind you at " + new DiscordTimestamp(date.getTime()).toLongDateTime() + " about:")
                     .addEmbeds(new EmbedBuilder().setDescription(description).build())
@@ -68,6 +74,7 @@ public class UserCommands extends ApplicationCommand {
                     .queue();
             reminder.schedule();
         } else {
+            // If the reminder could not be saved, send an error message and log the error too
             event.reply("Could not save your reminder, please try again later!").queue();
             log.error("Could not save reminder: " + reminder + " for user: " + event.getUser().getId() + " (" + result + ")");
         }
@@ -108,6 +115,7 @@ public class UserCommands extends ApplicationCommand {
             UUID parsed = UUID.fromString(uuid);
             Reminder reminder = NerdBotApp.getBot().getDatabase().findDocument(NerdBotApp.getBot().getDatabase().getCollection("reminders", Reminder.class), Filters.and(Filters.eq("userId", event.getUser().getId()), Filters.eq("uuid", parsed))).first();
 
+            // Check if the reminder exists first
             if (reminder == null) {
                 event.reply("Could not find reminder: " + uuid).setEphemeral(true).queue();
                 return;
@@ -115,6 +123,7 @@ public class UserCommands extends ApplicationCommand {
 
             DeleteResult result = reminder.delete();
 
+            // Check if the reminder was deleted successfully, cancel the timer and send a confirmation message
             if (result != null && result.wasAcknowledged()) {
                 if (reminder.getTimer() != null) {
                     reminder.getTimer().cancel();
@@ -123,6 +132,7 @@ public class UserCommands extends ApplicationCommand {
                 event.reply("Deleted reminder: " + uuid + " (" + reminder.getDescription() + ")").setEphemeral(true).queue();
                 log.info("Deleted reminder: " + uuid + " for user: " + event.getUser().getId());
             } else {
+                // If the reminder could not be deleted, send an error message and log the error
                 event.reply("Could not delete reminder: " + uuid).setEphemeral(true).queue();
                 log.info("Could not delete reminder " + uuid + " for user: " + event.getUser().getId() + " (" + result + ")");
             }
@@ -131,6 +141,13 @@ public class UserCommands extends ApplicationCommand {
         }
     }
 
+    /**
+     * Parse a time string in the format of {@code 1w2d3h4m5s} into a Date
+     *
+     * @param time The time string to parse
+     * @return The parsed string as a Date
+     * @throws DateTimeParseException If the string could not be parsed
+     */
     public static Date parseCustomFormat(String time) throws DateTimeParseException {
         Pattern pattern = Pattern.compile("((\\d+)w)?((\\d+)d)?((\\d+)h)?((\\d+)m)?((\\d+)s)?");
         Matcher matcher = pattern.matcher(time);
