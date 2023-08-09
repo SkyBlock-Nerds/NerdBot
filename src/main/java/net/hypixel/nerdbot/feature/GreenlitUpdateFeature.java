d8dac7509f9c6f9eac1c40bd2c82fb21fa63befa
Aerhhh <callum@aerh.net>
2023-08-09 22:48:27 +0100

    Run GreenlitUpdateFeature in its own thread

--- src/main/java/net/hypixel/nerdbot/feature/GreenlitUpdateFeature.java
+++ src/main/java/net/hypixel/nerdbot/feature/GreenlitUpdateFeature.java
@@ -38,112 +38,114 @@ public class GreenlitUpdateFeature extends BotFeature {
         this.timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                Stream<ForumChannel> suggestions = Util.safeArrayStream(channelConfig.getSuggestionForumIds(), channelConfig.getAlphaSuggestionForumIds())
-                    .map(s -> NerdBotApp.getBot().getJDA().getForumChannelById(s))
-                    .filter(Objects::nonNull);
+                NerdBotApp.EXECUTOR_SERVICE.submit(() -> {
+                    Stream<ForumChannel> suggestions = Util.safeArrayStream(channelConfig.getSuggestionForumIds(), channelConfig.getAlphaSuggestionForumIds())
+                        .map(s -> NerdBotApp.getBot().getJDA().getForumChannelById(s))
+                        .filter(Objects::nonNull);
 
-                suggestions.forEach(channel -> {
-                    boolean alpha;
+                    suggestions.forEach(channel -> {
+                        boolean alpha;
 
-                    if (channelConfig.getAlphaSuggestionForumIds() != null) {
-                        alpha = Arrays.asList(channelConfig.getAlphaSuggestionForumIds()).contains(channel.getId());
-                    } else {
-                        alpha = channel.getName().contains("alpha");
-                    }
+                        if (channelConfig.getAlphaSuggestionForumIds() != null) {
+                            alpha = Arrays.asList(channelConfig.getAlphaSuggestionForumIds()).contains(channel.getId());
+                        } else {
+                            alpha = channel.getName().contains("alpha");
+                        }
 
-                    log.info("Processing" + (alpha ? " alpha" : "") + " suggestion forum channel '" + channel.getName() + "' (ID: " + channel.getId() + ")");
+                        log.info("Processing" + (alpha ? " alpha" : "") + " suggestion forum channel '" + channel.getName() + "' (ID: " + channel.getId() + ")");
 
-                    List<ThreadChannel> threads = new ArrayList<>(channel.getThreadChannels()
-                        .stream()
-                        .filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).anyMatch(tag -> Arrays.asList(INCLUDED_TAGS).contains(tag)))
-                        .toList());
-                    List<ThreadChannel> archived = channel.retrieveArchivedPublicThreadChannels().complete();
-
-                    threads.addAll(
-                        archived.stream()
-                            .filter(threadChannel -> !threads.contains(threadChannel))
+                        List<ThreadChannel> threads = new ArrayList<>(channel.getThreadChannels()
+                            .stream()
                             .filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).anyMatch(tag -> Arrays.asList(INCLUDED_TAGS).contains(tag)))
-                            .toList()
-                    );
+                            .toList());
+                        List<ThreadChannel> archived = channel.retrieveArchivedPublicThreadChannels().complete();
+
+                        threads.addAll(
+                            archived.stream()
+                                .filter(threadChannel -> !threads.contains(threadChannel))
+                                .filter(threadChannel -> threadChannel.getAppliedTags().stream().map(ForumTag::getName).anyMatch(tag -> Arrays.asList(INCLUDED_TAGS).contains(tag)))
+                                .toList()
+                        );
 
 
-                    if (threads.isEmpty()) {
-                        log.info("No greenlit threads found in the suggestion forum channel!");
-                        return;
-                    }
-
-                    Database database = NerdBotApp.getBot().getDatabase();
-                    if (!database.isConnected()) {
-                        log.info("Database is not connected, skipping greenlit message update!");
-                        return;
-                    }
-
-                    List<GreenlitMessage> greenlits = database.getCollection("greenlit_messages", GreenlitMessage.class).find().into(new ArrayList<>());
-                    if (greenlits.isEmpty()) {
-                        log.info("No greenlit messages found in the database to update!");
-                        return;
-                    }
-
-                    log.info("Found " + channel.getThreadChannels().size() + " threads in the suggestion forum channel!");
-                    log.info("Found " + threads.size() + " unarchived greenlit threads in the suggestion forum channel!");
-                    log.info("Found " + archived.size() + " archived threads in the suggestion forum channel!");
-
-                    greenlits.forEach(greenlitMessage -> {
-                        log.info("Processing greenlit message '" + greenlitMessage.getSuggestionTitle() + "' (ID: " + greenlitMessage.getMessageId() + ")");
-                        ThreadChannel thread = threads.stream().filter(threadChannel -> threadChannel.getId().equals(greenlitMessage.getMessageId()) || threadChannel.getName().equalsIgnoreCase(greenlitMessage.getSuggestionTitle())).findFirst().orElse(null);
-                        if (thread == null) {
-                            log.warn("Couldn't find thread for greenlit message '" + greenlitMessage.getSuggestionTitle() + "' (ID: " + greenlitMessage.getMessageId() + ")!");
+                        if (threads.isEmpty()) {
+                            log.info("No greenlit threads found in the suggestion forum channel!");
                             return;
                         }
 
-                        log.info("Found matching thread for greenlit message " + greenlitMessage.getMessageId() + ": '" + thread.getName() + "' (ID: " + thread.getId() + ")");
+                        Database database = NerdBotApp.getBot().getDatabase();
+                        if (!database.isConnected()) {
+                            log.info("Database is not connected, skipping greenlit message update!");
+                            return;
+                        }
 
-                        thread.getHistoryFromBeginning(1).queue(messageHistory -> {
-                            log.info("Retrieved history for thread '" + thread.getName() + "' (ID: " + thread.getId() + ")");
-                            Message message = messageHistory.getRetrievedHistory().get(0);
-                            if (message == null) {
-                                log.error("Message for thread '" + thread.getName() + "' (ID: " + thread.getId() + ") is null!");
+                        List<GreenlitMessage> greenlits = database.getCollection("greenlit_messages", GreenlitMessage.class).find().into(new ArrayList<>());
+                        if (greenlits.isEmpty()) {
+                            log.info("No greenlit messages found in the database to update!");
+                            return;
+                        }
+
+                        log.info("Found " + channel.getThreadChannels().size() + " threads in the suggestion forum channel!");
+                        log.info("Found " + threads.size() + " unarchived greenlit threads in the suggestion forum channel!");
+                        log.info("Found " + archived.size() + " archived threads in the suggestion forum channel!");
+
+                        greenlits.forEach(greenlitMessage -> {
+                            log.info("Processing greenlit message '" + greenlitMessage.getSuggestionTitle() + "' (ID: " + greenlitMessage.getMessageId() + ")");
+                            ThreadChannel thread = threads.stream().filter(threadChannel -> threadChannel.getId().equals(greenlitMessage.getMessageId()) || threadChannel.getName().equalsIgnoreCase(greenlitMessage.getSuggestionTitle())).findFirst().orElse(null);
+                            if (thread == null) {
+                                log.warn("Couldn't find thread for greenlit message '" + greenlitMessage.getSuggestionTitle() + "' (ID: " + greenlitMessage.getMessageId() + ")!");
                                 return;
                             }
 
-                            List<MessageReaction> reactions = message.getReactions()
-                                .stream()
-                                .filter(reaction -> reaction.getEmoji().getType() == Emoji.Type.CUSTOM)
-                                .toList();
+                            log.info("Found matching thread for greenlit message " + greenlitMessage.getMessageId() + ": '" + thread.getName() + "' (ID: " + thread.getId() + ")");
 
-                            Map<String, Integer> votes = Stream.of(
-                                    emojiConfig.getAgreeEmojiId(),
-                                    emojiConfig.getNeutralEmojiId(),
-                                    emojiConfig.getDisagreeEmojiId()
-                                )
-                                .map(emojiId -> Pair.of(
-                                    emojiId,
-                                    reactions.stream()
-                                        .filter(reaction -> reaction.getEmoji()
-                                            .asCustom()
-                                            .getId()
-                                            .equalsIgnoreCase(emojiId)
-                                        )
-                                        .mapToInt(MessageReaction::getCount)
-                                        .findFirst()
-                                        .orElse(0)
-                                ))
-                                .collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
+                            thread.getHistoryFromBeginning(1).queue(messageHistory -> {
+                                log.info("Retrieved history for thread '" + thread.getName() + "' (ID: " + thread.getId() + ")");
+                                Message message = messageHistory.getRetrievedHistory().get(0);
+                                if (message == null) {
+                                    log.error("Message for thread '" + thread.getName() + "' (ID: " + thread.getId() + ") is null!");
+                                    return;
+                                }
 
-                            int agree = votes.get(emojiConfig.getAgreeEmojiId());
-                            int neutral = votes.get(emojiConfig.getNeutralEmojiId());
-                            int disagree = votes.get(emojiConfig.getDisagreeEmojiId());
-                            greenlitMessage.setSuggestionTitle(thread.getName());
-                            greenlitMessage.setSuggestionContent(message.getContentRaw());
-                            greenlitMessage.setTags(thread.getAppliedTags().stream().map(BaseForumTag::getName).toList());
-                            greenlitMessage.setAgrees(agree);
-                            greenlitMessage.setNeutrals(neutral);
-                            greenlitMessage.setDisagrees(disagree);
-                            greenlitMessage.setNeutrals(neutral);
+                                List<MessageReaction> reactions = message.getReactions()
+                                    .stream()
+                                    .filter(reaction -> reaction.getEmoji().getType() == Emoji.Type.CUSTOM)
+                                    .toList();
 
-                            database.upsertDocument(database.getCollection("greenlit_messages", GreenlitMessage.class), "messageId", greenlitMessage.getMessageId(), greenlitMessage);
-                            log.info("Updated greenlit message '" + greenlitMessage.getSuggestionTitle() + "' (ID: " + greenlitMessage.getMessageId() + ") in the database!");
-                        }, throwable -> log.error("Failed to retrieve history for thread '" + thread.getName() + "' (ID: " + thread.getId() + ")", throwable));
+                                Map<String, Integer> votes = Stream.of(
+                                        emojiConfig.getAgreeEmojiId(),
+                                        emojiConfig.getNeutralEmojiId(),
+                                        emojiConfig.getDisagreeEmojiId()
+                                    )
+                                    .map(emojiId -> Pair.of(
+                                        emojiId,
+                                        reactions.stream()
+                                            .filter(reaction -> reaction.getEmoji()
+                                                .asCustom()
+                                                .getId()
+                                                .equalsIgnoreCase(emojiId)
+                                            )
+                                            .mapToInt(MessageReaction::getCount)
+                                            .findFirst()
+                                            .orElse(0)
+                                    ))
+                                    .collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
+
+                                int agree = votes.get(emojiConfig.getAgreeEmojiId());
+                                int neutral = votes.get(emojiConfig.getNeutralEmojiId());
+                                int disagree = votes.get(emojiConfig.getDisagreeEmojiId());
+                                greenlitMessage.setSuggestionTitle(thread.getName());
+                                greenlitMessage.setSuggestionContent(message.getContentRaw());
+                                greenlitMessage.setTags(thread.getAppliedTags().stream().map(BaseForumTag::getName).toList());
+                                greenlitMessage.setAgrees(agree);
+                                greenlitMessage.setNeutrals(neutral);
+                                greenlitMessage.setDisagrees(disagree);
+                                greenlitMessage.setNeutrals(neutral);
+
+                                database.upsertDocument(database.getCollection("greenlit_messages", GreenlitMessage.class), "messageId", greenlitMessage.getMessageId(), greenlitMessage);
+                                log.info("Updated greenlit message '" + greenlitMessage.getSuggestionTitle() + "' (ID: " + greenlitMessage.getMessageId() + ") in the database!");
+                            }, throwable -> log.error("Failed to retrieve history for thread '" + thread.getName() + "' (ID: " + thread.getId() + ")", throwable));
+                        });
                     });
                 });
             }
