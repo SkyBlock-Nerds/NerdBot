0affd45736a266cc7e45a1901411fd4aec5dda9c
Aerhhh <callum@aerh.net>
2023-08-16 20:45:03 +0100

    Update ConfigGenerator

--- src/main/java/net/hypixel/nerdbot/bot/config/generator/ConfigGenerator.java
+++ src/main/java/net/hypixel/nerdbot/bot/config/generator/ConfigGenerator.java
@@ -1,6 +1,8 @@
 package net.hypixel.nerdbot.bot.config.generator;
 
-import net.hypixel.nerdbot.NerdBotApp;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import net.hypixel.nerdbot.bot.config.BotConfig;
 
 import java.io.FileWriter;
@@ -9,17 +11,39 @@ import java.io.IOException;
 public class ConfigGenerator {
 
     public static void main(String[] args) {
+        Gson gson = new Gson();
         BotConfig botConfig = new BotConfig();
-        String json = NerdBotApp.GSON.toJson(botConfig);
+        String json = gson.toJson(botConfig);
 
+        if (isValidJson(json)) {
+            System.out.println("The provided JSON string is valid!");
+
+            try {
+                writeJsonToFile(json);
+            } catch (IOException e) {
+                System.err.println("Error writing JSON to file:");
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("The provided JSON string is invalid!");
+            System.exit(-1);
+        }
+    }
+
+    private static boolean isValidJson(String jsonStr) {
+        try {
+            JsonParser.parseString(jsonStr);
+            return true;
+        } catch (JsonSyntaxException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void writeJsonToFile(String json) throws IOException {
         try (FileWriter writer = new FileWriter("./src/main/resources/example-config.json")) {
             writer.write(json);
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Created JSON file successfully!");
         }
-
-        // Check if the generated config is valid
-        BotConfig generatedConfig = NerdBotApp.GSON.fromJson(json, BotConfig.class);
-        System.out.println(generatedConfig);
     }
 }
