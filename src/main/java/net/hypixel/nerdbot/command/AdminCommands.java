27345a6f699aa9f7530cf9fdd9966a215dfc976b
Aerhhh <callum@aerh.net>
2023-08-18 17:51:55 +0100

    Add /flared as a slash command

--- src/main/java/net/hypixel/nerdbot/command/AdminCommands.java
+++ src/main/java/net/hypixel/nerdbot/command/AdminCommands.java
@@ -13,8 +13,11 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.IMentionable;
 import net.dv8tion.jda.api.entities.Invite;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.forums.ForumTag;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.api.requests.restaction.InviteAction;
 import net.dv8tion.jda.api.utils.FileUpload;
@@ -173,7 +176,7 @@ public class AdminCommands extends ApplicationCommand {
     public void userList(GuildSlashEvent event, @Optional @AppOption(description = "Comma-separated role names to search for (default Member).") String roles) throws IOException {
         event.deferReply(true).complete();
         Database database = NerdBotApp.getBot().getDatabase();
-        String[] roleArray = roles != null ? roles.split(", ?") : new String[] { "Member" };
+        String[] roleArray = roles != null ? roles.split(", ?") : new String[]{"Member"};
         JsonArray uuidArray = new JsonArray();
 
         List<MojangProfile> profiles = event.getGuild()
@@ -363,5 +366,30 @@ public class AdminCommands extends ApplicationCommand {
             }
         });
         event.getHook().editOriginal("Updated nicknames for " + users.into(new ArrayList<>()).size() + " users.").queue();
+    }
+
+    @JDASlashCommand(name = "flared", description = "Add the Flared tag to a suggestion and lock it", defaultLocked = true)
+    public void flareSuggestion(GuildSlashEvent event) {
+        Channel channel = event.getChannel();
+
+        if (!(channel instanceof ThreadChannel threadChannel) || !(threadChannel.getParentChannel() instanceof ForumChannel forumChannel)) {
+            event.getHook().sendMessage("This command can only be used inside a thread that is part of a forum!").queue();
+            return;
+        }
+
+        ForumTag forumTag = forumChannel.getAvailableTagsByName("flared", true).get(0);
+
+        if (forumTag == null) {
+            event.getHook().sendMessage("Couldn't find a Flared tag in this forum channel!").queue();
+            return;
+        }
+
+        List<ForumTag> appliedTags = new ArrayList<>(threadChannel.getAppliedTags());
+        appliedTags.add(forumTag);
+
+        threadChannel.getManager()
+            .setLocked(true)
+            .setAppliedTags(appliedTags)
+            .queue();
     }
 }
