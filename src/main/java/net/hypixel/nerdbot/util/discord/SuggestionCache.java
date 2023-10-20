0bd3e3df43b7da3d0cbe48c5e8cfe682136980fb
Aerh <22944369+Aerhhh@users.noreply.github.com>
2023-10-20 04:34:09 +0100

    Hijack SuggestionCache to retroactively store reaction history

--- src/main/java/net/hypixel/nerdbot/util/discord/SuggestionCache.java
+++ src/main/java/net/hypixel/nerdbot/util/discord/SuggestionCache.java
@@ -8,18 +8,13 @@ import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.hypixel.nerdbot.NerdBotApp;
+import net.hypixel.nerdbot.api.database.model.user.DiscordUser;
+import net.hypixel.nerdbot.api.database.model.user.stats.ReactionHistory;
 import net.hypixel.nerdbot.bot.config.ChannelConfig;
 import net.hypixel.nerdbot.util.Util;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.stream.Stream;
 
 @Log4j2
@@ -52,8 +47,21 @@ public class SuggestionCache extends TimerTask {
                 .distinct()
                 .forEach(thread -> {
                     this.cache.put(thread.getId(), new Suggestion(thread));
-                    //User user = NerdBotApp.getBot().getJDA().getUserById(thread.getOwnerIdLong());
-                    //log.info("Added existing suggestion: '" + thread.getName() + "' (ID: " + thread.getId() + ") (User: " + (user != null ? user.getEffectiveName() + "/" : "") + thread.getOwnerId() + ") to the suggestion cache.");
+                    log.debug("Added existing suggestion: '" + thread.getName() + "' (ID: " + thread.getId() + ") to the suggestion cache.");
+
+                    Message startMessage = thread.retrieveStartMessage().complete();
+                    startMessage.getReactions().stream()
+                        .filter(messageReaction -> messageReaction.getEmoji().asCustom().getId().equalsIgnoreCase(NerdBotApp.getBot().getConfig().getEmojiConfig().getAgreeEmojiId()))
+                        .forEach(messageReaction -> {
+                        messageReaction.retrieveUsers().complete().forEach(user -> {
+                            DiscordUser discordUser = Util.getOrAddUserToCache(NerdBotApp.getBot().getDatabase(), user.getId());
+                            List<ReactionHistory> reactionHistory = discordUser.getLastActivity().getSuggestionReactionHistory();
+
+                            reactionHistory.removeIf(history -> history.reactionName().equals(messageReaction.getEmoji().getName()) && history.channelId().equals(thread.getId()));
+                            discordUser.getLastActivity().getSuggestionReactionHistory().add(new ReactionHistory(thread.getId(), messageReaction.getEmoji().getName(), thread.getTimeCreated().toEpochSecond()));
+                            log.debug("Added reaction history for user '" + user.getId() + "' on suggestion '" + thread.getName() + "' (ID: " + thread.getId() + ")");
+                        });
+                    });
                 });
 
             log.info("Removing expired suggestions.");
