ee2ec23f31240b8be0e107e36c0709bb70ac8442
Aerh <22944369+Aerhhh@users.noreply.github.com>
2023-11-02 21:35:35 +0000

    Change ClassUtil

--- src/main/java/net/hypixel/nerdbot/util/ClassUtil.java
+++ src/main/java/net/hypixel/nerdbot/util/ClassUtil.java
@@ -2,28 +2,44 @@ package net.hypixel.nerdbot.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ClassUtil {
 
-    public static List<Class<?>> getClassesInPackage(String packageName) throws IOException, ClassNotFoundException {
+    private static final Map<String, List<Class<?>>> packageClassCache = new ConcurrentHashMap<>();
+
+    private ClassUtil() {
+    }
+
+    public static List<Class<?>> getClassesInPackage(String packageName) throws IOException, ClassNotFoundException, URISyntaxException {
+        List<Class<?>> cachedClasses = packageClassCache.get(packageName);
+        if (cachedClasses != null) {
+            return cachedClasses;
+        }
+
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        String path = packageName.replace('.', '/');
+        String path = packageName.replace('.', File.separatorChar);
         Enumeration<URL> resources = classLoader.getResources(path);
         List<File> dirs = new ArrayList<>();
         List<Class<?>> classes = new ArrayList<>();
 
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
+            dirs.add(new File(resource.toURI()));
         }
 
         for (File directory : dirs) {
             classes.addAll(findClasses(directory, packageName));
         }
+
+        // Cache the result for this package
+        packageClassCache.put(packageName, classes);
 
         return classes;
     }
