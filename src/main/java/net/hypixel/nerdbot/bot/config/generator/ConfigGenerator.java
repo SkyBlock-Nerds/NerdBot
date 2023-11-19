508c690f458494f59e9192dfdb3d7146dc3d41f1
Aerh <22944369+Aerhhh@users.noreply.github.com>
2023-11-19 02:48:36 +0000

    oops

--- src/main/java/net/hypixel/nerdbot/bot/config/generator/ConfigGenerator.java
+++ src/main/java/net/hypixel/nerdbot/bot/config/generator/ConfigGenerator.java
@@ -31,7 +31,7 @@ public class ConfigGenerator {
         }
     }
 
-    public static boolean isValidJson(String jsonStr) {
+    private static boolean isValidJson(String jsonStr) {
         try {
             JsonParser.parseString(jsonStr);
             return true;
@@ -41,7 +41,7 @@ public class ConfigGenerator {
         }
     }
 
-    public static void writeJsonToFile(String json) throws IOException {
+    private static void writeJsonToFile(String json) throws IOException {
         try (FileWriter writer = new FileWriter("./src/main/resources/example-config.json")) {
             writer.write(json);
             System.out.println("Created JSON file successfully!");
