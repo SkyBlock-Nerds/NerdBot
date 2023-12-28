3a34338a24d44466649034b52c31bb86b791e879
Aerhhh <22944369+Aerhhh@users.noreply.github.com>
2023-12-28 23:44:29 +0000

    Why can't people just use the new format
    (cherry picked from commit fabdfec0bfa92eb50a5a7d275579574e199e32c3)

--- src/main/java/net/hypixel/nerdbot/command/MyCommands.java
+++ src/main/java/net/hypixel/nerdbot/command/MyCommands.java
@@ -308,8 +308,7 @@ public class MyCommands extends ApplicationCommand {
         String discord = hypixelPlayerResponse.getPlayer().getSocialMedia().getLinks().get(HypixelPlayerResponse.SocialMedia.Service.DISCORD);
         String discordName = member.getUser().getName();
 
-        // Apparently there's two different formats
-        if (!member.getUser().getDiscriminator().equalsIgnoreCase("0") || !member.getUser().getDiscriminator().equalsIgnoreCase("0000")) {
+        if (!member.getUser().getDiscriminator().equalsIgnoreCase("0000")) {
             discordName += "#" + member.getUser().getDiscriminator();
         }
 
