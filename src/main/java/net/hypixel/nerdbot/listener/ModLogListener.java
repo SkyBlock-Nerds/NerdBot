9b2647ad68b94d792fa8360fb4e189ae3d07f476
Aerh <22944369+Aerhhh@users.noreply.github.com>
2024-01-17 04:29:37 +0000

    Math.min deleted content
    (cherry picked from commit 06f92e4932380402faa74350e4a0769c92dc3abc)

--- src/main/java/net/hypixel/nerdbot/listener/ModLogListener.java
+++ src/main/java/net/hypixel/nerdbot/listener/ModLogListener.java
@@ -205,7 +205,7 @@ public class ModLogListener {
             .addField("User", user.getAsMention(), false)
             .addField("Channel", channel.getAsMention(), false)
             .addField("User ID", user.getId(), false)
-            .addField("Content", message.getContentDisplay().substring(0, 2_000), false);
+            .addField("Content", message.getContentDisplay().substring(0, Math.min(message.getContentDisplay().length(), 2_000)), false);
 
         if (!message.getAttachments().isEmpty()) {
             StringBuilder attachments = new StringBuilder();
