e9d471f1af39bd98d7e9767a12b963ee745f6e6b
Aerh <22944369+Aerhhh@users.noreply.github.com>
2024-01-20 01:31:40 +0000

    Update src/main/java/net/hypixel/nerdbot/api/database/model/user/UserLanguage.java

--- src/main/java/net/hypixel/nerdbot/api/database/model/user/UserLanguage.java
+++ src/main/java/net/hypixel/nerdbot/api/database/model/user/UserLanguage.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.Nullable;
 
 @Getter
 public enum UserLanguage {
-    ENGLISH("English", "english.json");
+    ENGLISH("English", "english.json"),
     DUTCH("Dutch", "dutch.json");
 
     public static final UserLanguage[] VALUES = values();
