7372558bfcb1f77c1d870abfc06d311cf93b5541
Aerh <22944369+Aerhhh@users.noreply.github.com>
2024-01-28 01:22:02 +0000

    Update DEFAULT_SKIN_VALUE

--- src/main/java/net/hypixel/nerdbot/generator/impl/MinecraftPlayerHeadGenerator.java
+++ src/main/java/net/hypixel/nerdbot/generator/impl/MinecraftPlayerHeadGenerator.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MinecraftPlayerHeadGenerator implements Generator {
 
-    private static final String DEFAULT_SKIN_VALUE = "Steve"; // TODO find better value
+    private static final String DEFAULT_SKIN_VALUE = "31f477eb1a7beee631c2ca64d06f8f68fa93a3386d04452ab27f43acdf1b60cb"; // TODO find better value
     private static final Pattern TEXTURE_URL = Pattern.compile("(?:https?://textures.minecraft.net/texture/)?([a-zA-Z0-9]+)");
 
     private final String textureId;
