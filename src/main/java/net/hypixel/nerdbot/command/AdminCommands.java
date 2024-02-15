a3f486c780f275b38bdf1c575a685e60c49fe218
Aerh <22944369+Aerhhh@users.noreply.github.com>
2024-02-15 06:25:27 +0000

    Remove greenlit tag in exporter

--- src/main/java/net/hypixel/nerdbot/command/AdminCommands.java
+++ src/main/java/net/hypixel/nerdbot/command/AdminCommands.java
@@ -194,12 +194,14 @@ public class AdminCommands extends ApplicationCommand {
                 if (greenlitMessage.getSuggestionTimestamp() >= suggestionsAfter) {
                     // The Format is shown below, Tabs (\t) are the separators between values, as commas cannot be used, but It's still in the CSV file format due to Google Sheets Default Import only accepting CSV files.
                     // Timestamp Posted, Tags, Suggestion Title (Hyperlinked to the post), Reserved Location, Reserved Location, Reserved Location, Reserved Location, Reserved Location
+                    greenlitMessage.getTags().removeIf(tag -> tag.equalsIgnoreCase("greenlit"));
+
                     csvOutput.append("=EPOCHTODATE(").append(greenlitMessage.getSuggestionTimestamp() / 1_000L)
                         .append(")\t")
                         .append(String.join(", ", greenlitMessage.getTags()))
                         .append("\t=HYPERLINK(\"")
                         .append(greenlitMessage.getSuggestionUrl()).append("\", \"")
-                        .append(greenlitMessage.getSuggestionTitle()).append("\")")
+                        .append(greenlitMessage.getSuggestionTitle().replaceAll("\"", "\"\"")).append("\")")
                         .append("\t\t\t\t\t\t")
                         .append("\n");
                 }
