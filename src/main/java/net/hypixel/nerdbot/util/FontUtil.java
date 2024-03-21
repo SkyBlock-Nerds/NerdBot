f17580508b01b7da79057cb6c6390edf6d0feb6f
Aerh <22944369+Aerhhh@users.noreply.github.com>
2024-03-21 01:15:28 +0000

    more debug

--- src/main/java/net/hypixel/nerdbot/util/FontUtil.java
+++ src/main/java/net/hypixel/nerdbot/util/FontUtil.java
@@ -1,7 +1,6 @@
 package net.hypixel.nerdbot.util;
 
 import java.awt.Font;
-import java.awt.Shape;
 import java.awt.font.FontRenderContext;
 import java.awt.font.GlyphVector;
 
@@ -9,16 +8,17 @@ public class FontUtil {
 
     /**
      * Checks if a font can render a character.
-     * @param font The font to check.
+     *
+     * @param font      The font to check.
      * @param character The character to check.
+     *
      * @return True if the font can render the character, false otherwise.
      */
     public static boolean canRenderCharacter(Font font, char character) {
         FontRenderContext frc = new FontRenderContext(null, true, true);
         GlyphVector gv = font.createGlyphVector(frc, Character.toString(character));
-        Shape shape = gv.getOutline();
 
-        System.out.println("Font " + font.getName() + " can render " + character + ": " + !shape.getBounds().isEmpty());
-        return !shape.getBounds().isEmpty();
+        System.out.println("Font: " + font.getFontName() + ", Glyphs: " + gv.getNumGlyphs() + ", Code: " + gv.getGlyphCode(0));
+        return gv.getNumGlyphs() == 1 && gv.getGlyphCode(0) == 0x25A1; // Unicode for empty box character
     }
 }
