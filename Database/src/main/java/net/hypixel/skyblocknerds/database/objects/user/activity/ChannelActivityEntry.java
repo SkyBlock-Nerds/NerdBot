4935b6109eb6d6c829050b0ef7ad5c2ace9bb074
Aerhhh <22944369+Aerhhh@users.noreply.github.com>
2024-05-24 00:23:10 +0100

    Comments

--- Database/src/main/java/net/hypixel/skyblocknerds/database/objects/user/activity/ChannelActivityEntry.java
+++ Database/src/main/java/net/hypixel/skyblocknerds/database/objects/user/activity/ChannelActivityEntry.java
@@ -11,10 +11,32 @@ import java.util.Map;
 @Setter
 public class ChannelActivityEntry {
 
+    /**
+     * The ID of the channel where the activity entry took place
+     */
     private String channelId;
+
+    /**
+     * The last known name of the channel where the activity entry took place
+     */
     private String lastKnownDisplayName;
+
+    /**
+     * The amount of messages sent in the channel
+     */
     private int messageCount;
+
+    /**
+     * The {@link Long timestamp} of the last message sent in the channel
+     */
     private long lastMessageTimestamp;
+
+    /**
+     * The amount of messages sent in the channel per month
+     * <p>
+     * The key is the MONTH-YEAR (e.g. 01-2021) and the value is the amount of messages sent in that month
+     * </p>
+     */
     private Map<String, Integer> monthlyMessageCount;
 
 }
