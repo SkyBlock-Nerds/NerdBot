145ab4640029e3e36ba075bc5aba98ffb51b70ca
Aerhhh <22944369+Aerhhh@users.noreply.github.com>
2024-05-25 20:20:35 +0100

    Create /warnings command

--- DiscordBot/src/main/java/net/hypixel/skyblocknerds/discordbot/command/ModerationCommands.java
+++ DiscordBot/src/main/java/net/hypixel/skyblocknerds/discordbot/command/ModerationCommands.java
@@ -9,6 +9,8 @@ import net.hypixel.skyblocknerds.database.objects.user.warning.WarningEntry;
 import net.hypixel.skyblocknerds.database.repository.RepositoryManager;
 import net.hypixel.skyblocknerds.database.repository.impl.DiscordUserRepository;
 
+import java.util.stream.Collectors;
+
 public class ModerationCommands extends ApplicationCommand {
 
     private final DiscordUserRepository discordUserRepository;
@@ -25,6 +27,20 @@ public class ModerationCommands extends ApplicationCommand {
             discordUser.getWarnings().add(new WarningEntry(reason, System.currentTimeMillis(), event.getMember().getId()));
             event.getHook().editOriginal("Warned " + member.getAsMention() + " for `" + reason + "`").queue();
             // TODO log the warning in a channel
+        }, () -> event.reply("Couldn't find that member in the database!").queue());
+    }
+
+    @JDASlashCommand(name = "warnings", description = "View the warnings of a member", defaultLocked = true)
+    public void viewWarnings(GuildSlashEvent event, @AppOption(description = "The member to view the warnings of") Member member) {
+        event.deferReply(true).complete();
+
+        discordUserRepository.findById(member.getId()).ifPresentOrElse(discordUser -> {
+            if (discordUser.getWarnings().isEmpty()) {
+                event.getHook().editOriginal(member.getAsMention() + " has no warnings!").queue();
+            } else {
+                String warnings = discordUser.getWarnings().stream().map(warning -> warning.getReason() + " - Issued by <@" + warning.getIssuerId() + ">\n").collect(Collectors.joining());
+                event.getHook().editOriginal(member.getAsMention() + "'s warnings:\n" + warnings).queue();
+            }
         }, () -> event.reply("Couldn't find that member in the database!").queue());
     }
 }
