6c6c4dc3c800dd5a2e85f0f0d8a4202a7e7c53d1
Aerhhh <22944369+Aerhhh@users.noreply.github.com>
2025-04-07 17:10:16 +0100

    Update spritesheet

--- src/main/java/net/hypixel/nerdbot/generator/spritesheet/SpritesheetGenerator.java
+++ src/main/java/net/hypixel/nerdbot/generator/spritesheet/SpritesheetGenerator.java
@@ -21,11 +21,11 @@ import java.util.List;
 
 public class SpritesheetGenerator {
 
-    public static final int IMAGE_HEIGHT = 16;
-    public static final int IMAGE_WIDTH = 16;
+    public static final int IMAGE_HEIGHT = 256;
+    public static final int IMAGE_WIDTH = 256;
     public static final int LARGE_IMAGE_WIDTH = 300;
     public static final int LARGE_IMAGE_HEIGHT = 300;
-    private static final int ATLAS_WIDTH = 1_024 * 4;
+    private static final int ATLAS_WIDTH = 1_024 * 8;
     private static final List<TextureInfo> textureInfo = new ArrayList<>();
 
     public static void main(String[] args) {
