597f7708c09aa003befabd62871905428831ef7a
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-10 22:28:14 +0200

    Case insensitive query params

--- src/main/java/net/hypixel/nerdbot/publicapi/WebConfig.java
+++ src/main/java/net/hypixel/nerdbot/publicapi/WebConfig.java
@@ -2,10 +2,20 @@ package net.hypixel.nerdbot.publicapi;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "net.hypixel.nerdbot.publicapi")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        matcher.setCaseSensitive(false);
+        configurer.setPathMatcher(matcher);
+    }
 }
