8104d66e3a4d2a9d152d4513f0669020d1557b57
Aerh <22944369+Aerhhh@users.noreply.github.com>
2025-12-13 17:37:57 +0000

    Pre-compile Color Regex in ColorCodeParser (#501)

--- generator/src/main/java/net/hypixel/nerdbot/generator/parser/text/ColorCodeParser.java
+++ generator/src/main/java/net/hypixel/nerdbot/generator/parser/text/ColorCodeParser.java
@@ -3,14 +3,23 @@ package net.hypixel.nerdbot.generator.parser.text;
 import net.hypixel.nerdbot.generator.parser.StringParser;
 import net.hypixel.nerdbot.generator.text.ChatFormat;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ColorCodeParser implements StringParser {
+
+    private static final Map<ChatFormat, Pattern> PATTERNS = Arrays.stream(ChatFormat.VALUES)
+        .collect(Collectors.toMap(
+            chatFormat -> chatFormat,
+            chatFormat -> Pattern.compile("%%" + chatFormat.name() + "%%", Pattern.CASE_INSENSITIVE)
+        ));
 
     @Override
     public String parse(String input) {
         for (ChatFormat value : ChatFormat.VALUES) {
-            Pattern pattern = Pattern.compile("%%" + value.name() + "%%", Pattern.CASE_INSENSITIVE);
+            Pattern pattern = PATTERNS.get(value);
             input = pattern.matcher(input).replaceAll(String.valueOf(ChatFormat.AMPERSAND_SYMBOL) + value.getCode());
         }
 
