4c5f24dc8ffc1cd2fccbb165ed12708b082d861f
Aerh <22944369+Aerhhh@users.noreply.github.com>
2026-02-15 18:22:55 +0000

    Separate Generator Classes (#541)

--- generator/src/main/java/net/hypixel/nerdbot/generator/exception/GeneratorValidationException.java
+++
@@ -1,8 +0,0 @@
-package net.hypixel.nerdbot.generator.exception;
-
-public class GeneratorValidationException extends GeneratorException {
-
-    public GeneratorValidationException(String message, String... formatArgs) {
-        super(message, formatArgs);
-    }
-}
--- generator/src/main/java/net/hypixel/nerdbot/generator/exception/NbtParseException.java
+++
@@ -1,8 +0,0 @@
-package net.hypixel.nerdbot.generator.exception;
-
-public class NbtParseException extends RuntimeException {
-
-    public NbtParseException(String message) {
-        super(message);
-    }
-}
